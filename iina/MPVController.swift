c441844f6488adc34b2b8a926649d7eb6b5f1ce6
顾佳奇 <droidqw@gmail.com>
2017-01-07 14:50:53 +0800

    New Feature And Fix bugs
    1.User can drag and drop the subtitle file to the IINA.
    2.Fix user-defined keybindings config file does not take effect.

--- iina/MPVController.swift
+++ iina/MPVController.swift
@@ -218,10 +218,16 @@ class MPVController: NSObject {
     let scriptPath = Bundle.main.path(forResource: "autoload", ofType: "lua", inDirectory: "scripts")!
     chkErr(mpv_set_option_string(mpv, MPVOption.ProgramBehavior.script, scriptPath))
     
-    let inputConfPath = Bundle.main.path(forResource: "input", ofType: "conf", inDirectory: "config")!
-    chkErr(mpv_set_option_string(mpv, MPVOption.Input.inputConf, inputConfPath))
-    
-    // Receive log messages at warn level.
+    //load keybinding
+    let userConfigs = UserDefaults.standard.dictionary(forKey: PK.inputConfigs)
+    var inputConfPath =  PrefKeyBindingViewController.defaultConfigs["MPV Default"]
+    if let confFromUd = UserDefaults.standard.string(forKey: PK.currentInputConfigName) {
+      let currentConfigFilePath = getFilePath(Configs: userConfigs, forConfig: confFromUd, showAlert: false)
+      if confFromUd != "MPV Default"  &&  currentConfigFilePath != nil {
+        inputConfPath = currentConfigFilePath!
+      }
+    }
+    chkErr(mpv_set_option_string(mpv, MPVOption.Input.inputConf, inputConfPath))    // Receive log messages at warn level.
     chkErr(mpv_request_log_messages(mpv, "warn"))
     
     // Request tick event.
@@ -243,6 +249,20 @@ class MPVController: NSObject {
     
     // get version
     mpvVersion = getString(MPVProperty.mpvVersion)
+  }
+  private func getFilePath(Configs userConfigs: [String: Any]!, forConfig conf: String, showAlert: Bool = true) -> String? {
+    
+    // if is default config
+    if let dv = PrefKeyBindingViewController.defaultConfigs[conf] {
+      return dv
+    } else if let uv = userConfigs[conf] as? String {
+      return uv
+    } else {
+      if showAlert {
+        Utility.showAlert(message: "Cannot find config file location!")
+      }
+      return nil
+    }
   }
   
   func mpvInitCB() -> UnsafeMutableRawPointer {

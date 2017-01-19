4bc4498f85d8103b5d12fb24b4b67f48242e4e82
Inflation <hypernovagama@gmail.com>
2017-01-18 22:58:22 -0500

    Add checking when parsing keybinding files
    - Add localization messages.

--- iina/PrefKeyBindingViewController.swift
+++ iina/PrefKeyBindingViewController.swift
@@ -301,6 +301,17 @@ class PrefKeyBindingViewController: NSViewController, MASPreferencesViewControll
       }
       // split
       let splitted = line.characters.split(separator: " ", maxSplits: 1)
+      if splitted.count < 2 {
+        let alert = String(format: NSLocalizedString("alert.keybinding_config_error", comment: "Error setting keybinding configuration"), currentConfName)
+        Utility.showAlert(message: alert)
+        let title = "IINA Default"
+        currentConfName = title
+        currentConfFilePath = getFilePath(forConfig: title)!
+        configSelectPopUp.selectItem(withTitle: title)
+        loadConfigFile()
+        changeButtonEnabled()
+        return
+      }
       let key = String(splitted[0])
       let action = splitted[1].split(separator: " ").map { seq in return String(seq) }
 

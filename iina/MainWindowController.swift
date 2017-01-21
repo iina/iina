8695dc9a349802a4d50033c17399bac19aebb760
Collider LI <lhc199652@gmail.com>
2017-01-21 13:35:00 +0800

    update timer when window open

--- iina/MainWindowController.swift
+++ iina/MainWindowController.swift
@@ -534,6 +534,8 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
     guard let w = self.window, let cv = w.contentView else { return }
     cv.addTrackingArea(NSTrackingArea(rect: cv.bounds, options: [.activeAlways, .inVisibleRect, .mouseEnteredAndExited, .mouseMoved], owner: self, userInfo: ["obj": 0]))
     playSlider.addTrackingArea(NSTrackingArea(rect: playSlider.bounds, options: [.activeAlways, .inVisibleRect, .mouseEnteredAndExited, .mouseMoved], owner: self, userInfo: ["obj": 1]))
+    // update timer
+    updateTimer()
   }
 
   func windowWillClose(_ notification: Notification) {
@@ -678,7 +680,7 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
     }
   }
 
-  private func showUI () {
+  private func showUI() {
     animationState = .willShow
     fadeableViews.forEach { (v) in
       v?.isHidden = false

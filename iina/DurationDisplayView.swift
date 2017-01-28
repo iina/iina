d5917cf93c9fa9d09be383f161c0e8b2a03af7bc
Chris Laprun <metacosm@gmail.com>
2017-01-28 15:50:00 +0100

    Fixed issue #283: duration label can now display remaining time when clicked. Implemented via DurationDisplayView, a custom NSTextField reacting to mouse down events. Added VideoTime minus operator to easily perform time differences.

---
+++ iina/DurationDisplayView.swift
@@ -0,0 +1,50 @@
+//
+//  DurationDisplayView.swift
+//  iina
+//
+//  Created by Christophe Laprun on 26/01/2017.
+//  Copyright © 2017 lhc. All rights reserved.
+//
+
+import Foundation
+
+class DurationDisplayView: NSTextField {
+
+  private enum DisplayMode {
+    case duration // displays the duration of the movie
+    case remaining // displays the remaining time in the movie
+  }
+
+  private var mode = DisplayMode.duration
+
+  /** Switches the display mode for the right label */
+  func switchMode() {
+    switch mode {
+    case .duration:
+      mode = .remaining
+    default:
+      mode = .duration
+    }
+  }
+
+
+  func updateText(with duration: VideoTime, given current: VideoTime) {
+
+    let stringValue: String
+    switch mode {
+    case .duration:
+      stringValue = duration.stringRepresentation
+    case .remaining:
+      let remaining = duration - current
+      stringValue = "-\(remaining.stringRepresentation)"
+    }
+    self.stringValue = stringValue
+  }
+
+  override func mouseDown(with event: NSEvent) {
+    super.mouseDown(with: event)
+
+    self.switchMode()
+  }
+
+}

a70e6540f8ee1b6a6adff0ee7edebf9022b23ca2
Collider LI <lhc199652@gmail.com>
2017-05-16 10:57:26 +0800

    --wip-- [skip ci]

---
+++ iina/SeriesDetector.swift
@@ -0,0 +1,22 @@
+//
+//  SeriesDetector.swift
+//  iina
+//
+//  Created by lhc on 15/5/2017.
+//  Copyright © 2017 lhc. All rights reserved.
+//
+
+import Foundation
+
+class SeriesDetector {
+
+  struct FileInfo {
+    var filename: String
+    var path: String
+  }
+
+  func detect(_ files: [FileInfo]) {
+
+  }
+
+}

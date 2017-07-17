1889a3ca164c1893f7aa54d4dd3b514851c1db9d
Collider LI <lhc199652@gmail.com>
2017-07-17 21:47:41 +0800

    fix wrong thumbnail position for top osc

--- iina/MainWindowController.swift
+++ iina/MainWindowController.swift
@@ -1526,8 +1526,8 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
         thumbnailPeekView.isHidden = false
         thumbnailPeekView.imageView.image = tb.image
         let height = round(120 / thumbnailPeekView.imageView.image!.size.aspect)
-        let yPos = (oscPosition == .top || (oscPosition == .floating && sliderFrameInWindow.y + 120 >= window!.frame.height)) ?
-          sliderFrameInWindow.y - 68 : sliderFrameInWindow.y + 32
+        let yPos = (oscPosition == .top || (oscPosition == .floating && sliderFrameInWindow.y + 52 + height >= window!.frame.height)) ?
+          sliderFrameInWindow.y - height : sliderFrameInWindow.y + 32
         thumbnailPeekView.frame.size = NSSize(width: 120, height: height)
         thumbnailPeekView.frame.origin = NSPoint(x: round(originalPos.x - thumbnailPeekView.frame.width / 2), y: yPos)
       } else {

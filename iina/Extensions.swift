d4c7bf02d1183322a30a7118bb14f57dccbbfde8
Yuze Jiang <fortissimoX@gmail.com>
2018-08-16 16:57:28 -0500

    `fabs` to `abs` to eliminate warnings

--- iina/Extensions.swift
+++ iina/Extensions.swift
@@ -152,8 +152,8 @@ extension NSRect {
   init(vertexPoint pt1: NSPoint, and pt2: NSPoint) {
     self.init(x: min(pt1.x, pt2.x),
               y: min(pt1.y, pt2.y),
-              width: fabs(pt1.x - pt2.x),
-              height: fabs(pt1.y - pt2.y))
+              width: abs(pt1.x - pt2.x),
+              height: abs(pt1.y - pt2.y))
   }
 
   func multiply(_ multiplier: CGFloat) -> NSRect {

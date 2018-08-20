88f61d2c5e462c132abb263ea661bd55ff74e466
Yuze Jiang <fortissimoX@gmail.com>
2018-08-20 16:25:44 -0500

    Additional bug fixes

--- iina/ScrollingTextField.swift
+++ iina/ScrollingTextField.swift
@@ -10,81 +10,77 @@ import Cocoa
 
 class ScrollingTextField: NSTextField {
 
+  enum State {
+    case idle
+    case pause
+    case scroll
+  }
+
+  private var state: State = .idle
+
   let frequency: TimeInterval = 0.03
 
-  private var isScrolling: Bool = false
-
-  private var scroller: Timer?
+  private var scrollingTimer: Timer?
+  private var pauseTimer: Timer?
   private var point: NSPoint = .zero
-
-  private var cachedStringWidth: CGFloat?
-  private var cachedStringValue: String?
 
   private var increment: CGFloat = 1
 
-  private var modifyInternally: Bool = false
+  private var scrollingString = NSAttributedString(string: "")
+  private var addedStringWidth: CGFloat = 0
 
   override var stringValue: String {
     didSet {
-      guard !modifyInternally else { return }
-      cachedStringValue = nil
       reset()
     }
   }
 
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-    startScrolling()
-  }
-
-  private func setStringValue(_ newValue: String) {
-    modifyInternally = true
-    stringValue = newValue
-    modifyInternally = false
-  }
-
   func scroll() {
     let stringWidth = attributedStringValue.size().width
-    guard !isScrolling && stringWidth > frame.maxX else { return }
+    guard state == .idle && stringWidth > 252 else { return }
 
-    isScrolling = true
-    Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(startScrolling), userInfo: nil, repeats: false)
+    state = .pause
+    pauseTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(startScrolling),
+                                      userInfo: nil, repeats: false)
   }
 
   @objc
   private func startScrolling() {
-    cachedStringValue = stringValue
-    setStringValue(stringValue + "   ")
-    cachedStringWidth = attributedStringValue.size().width
-    setStringValue(stringValue + cachedStringValue!)
-    scroller = Timer.scheduledTimer(timeInterval: frequency, target: self, selector: #selector(moveText), userInfo: nil, repeats: true)
+    let attributes = attributedStringValue.attributes(at: 0, effectiveRange: nil)
+    let addedString = stringValue + "    "
+    addedStringWidth = NSAttributedString(string: addedString, attributes: attributes).size().width
+    scrollingString = NSAttributedString(string: addedString + stringValue, attributes: attributes)
+    state = .scroll
+    scrollingTimer = Timer.scheduledTimer(timeInterval: frequency, target: self, selector: #selector(moveText),
+                                          userInfo: nil, repeats: true)
   }
 
   private func reset() {
-    scroller?.invalidate()
-    scroller = nil
-    setStringValue(cachedStringValue ?? stringValue)
-    cachedStringValue = nil
-    cachedStringWidth = nil
+    scrollingTimer?.invalidate()
+    scrollingTimer = nil
+    pauseTimer?.invalidate()
+    pauseTimer = nil
     point = .zero
-    isScrolling = false
+    state = .idle
     needsDisplay = true
   }
 
   @objc
   private func moveText() {
-    guard let cachedStringWidth = cachedStringWidth else { return }
     point.x -= increment
-    if point.x + cachedStringWidth < 0 {
+    if point.x + addedStringWidth < 0 {
       reset()
       return
     }
     needsDisplay = true
   }
 
-
   override func draw(_ dirtyRect: NSRect) {
-    attributedStringValue.draw(at: point)
+    if state == .scroll {
+      scrollingString.draw(at: point)
+    } else {
+      attributedStringValue.draw(at: point)
+    }
   }
 
 }

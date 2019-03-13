iina/iina

commit 83a2f8cc167d5963a157802b14c5e5ac4875a23a

Fix "VIDEO" tab button not bold when entering quicksetting by toolbar
Refactor bold transition related code & l10n clean up.

iina/PlaylistViewController.swift
@@ -205,18 +205,15 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
 
   /** Switch tab (for internal call) */
   private func switchToTab(_ tab: TabViewType) {
-    let playlistStr = NSLocalizedString("playlist.playlist_cap", comment: "PLAYLIST")
-    let chapterStr = NSLocalizedString("playlist.chapter_cap", comment: "CHAPTERS")
-
     switch tab {
     case .playlist:
       tabView.selectTabViewItem(at: 0)
-      playlistBtn.attributedTitle = NSAttributedString(string: playlistStr, attributes: Utility.tabTitleActiveFontAttributes)
-      chaptersBtn.attributedTitle = NSAttributedString(string: chapterStr, attributes: Utility.tabTitleFontAttributes)
+      Utility.setActive(playlistBtn, true)
+      Utility.setActive(chaptersBtn, false)
     case .chapters:
       tabView.selectTabViewItem(at: 1)
-      chaptersBtn.attributedTitle = NSAttributedString(string: chapterStr, attributes: Utility.tabTitleActiveFontAttributes)
-      playlistBtn.attributedTitle = NSAttributedString(string: playlistStr, attributes: Utility.tabTitleFontAttributes)
+      Utility.setActive(chaptersBtn, true)
+      Utility.setActive(playlistBtn, false)
     }
 
     currentTab = tab

iina/QuickSettingViewController.swift
@@ -282,33 +282,17 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
   /** Switch tab (for internal call) */
   private func switchToTab(_ tab: TabViewType) {
     let button: NSButton
-    let tabIndex: Int
     switch tab {
     case .video:
       button = videoTabBtn
-      tabIndex = 0
     case .audio:
       button = audioTabBtn
-      tabIndex = 1
     case .sub:
       button = subTabBtn
-      tabIndex = 2
     default:
       return
     }
-    tabView.selectTabViewItem(at: tabIndex)
-    // cancel current active button
-    for btn in [videoTabBtn, audioTabBtn, subTabBtn] {
-      if let btn = btn {
-        let title = btn.title
-        btn.attributedTitle = NSAttributedString(string: title, attributes: Utility.tabTitleFontAttributes)
-      }
-    }
-    // the active one
-    let title = button.title
-    button.attributedTitle = NSAttributedString(string: title, attributes: Utility.tabTitleActiveFontAttributes)
-
-    currentTab = tab
+    tabBtnAction(button)
   }
 
   // MARK: - NSTableView delegate
@@ -391,16 +375,8 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
 
   @IBAction func tabBtnAction(_ sender: NSButton) {
     tabView.selectTabViewItem(at: sender.tag)
-    // cancel current active button
-    [videoTabBtn, audioTabBtn, subTabBtn].forEach { btn in
-      if let btn = btn {
-        let title = btn.title
-        btn.attributedTitle = NSAttributedString(string: title, attributes: Utility.tabTitleFontAttributes)
-      }
-    }
-    // the active one
-    let title = sender.title
-    sender.attributedTitle = NSAttributedString(string: title, attributes: Utility.tabTitleActiveFontAttributes)
+    [videoTabBtn, audioTabBtn, subTabBtn].forEach { Utility.setActive($0, false) }
+    Utility.setActive(sender, true)
     reload()
   }
 

iina/Utility.swift
@@ -10,9 +10,6 @@ import Cocoa
 
 class Utility {
 
-  static let tabTitleFontAttributes = FontAttributes(font: .system, size: .system, align: .center).value
-  static let tabTitleActiveFontAttributes = FontAttributes(font: .systemBold, size: .system, align: .center).value
-
   static let supportedFileExt: [MPVTrack.TrackType: [String]] = [
     .video: ["mkv", "mp4", "avi", "m4v", "mov", "3gp", "ts", "mts", "m2ts", "wmv", "flv", "f4v", "asf", "webm", "rm", "rmvb", "qt", "dv", "mpg", "mpeg", "mxf", "vob", "gif"],
     .audio: ["mp3", "aac", "mka", "dts", "flac", "ogg", "oga", "mogg", "m4a", "ac3", "opus", "wav", "wv", "aiff", "ape", "tta", "tak"],
@@ -386,6 +383,11 @@ class Utility {
 
 
   // MARK: - Util functions
+
+  static func setActive(_ button: NSButton, _ active: Bool) {
+    button.attributedTitle = NSAttributedString(string: button.title,
+                                                attributes: FontAttributes(font: active ? .systemBold : .system, size: .system, align: .center).value)
+  }
 
   static func toRealSubScale(fromDisplaySubScale scale: Double) -> Double {
     return scale > 0 ? scale : -1 / scale

iina/Base.lproj/Localizable.strings
@@ -16,10 +16,6 @@
 "general.username" = "Username";
 "general.password" = "Password";
 
-// PlaylistViewController.swift
-"playlist.playlist_cap" = "PLAYLIST";
-"playlist.chapter_cap" = "CHAPTERS";
-
 // MainWindowController.swift
 "main.buffering_indicator" = "Buffering… %d%%";
 "main.opening_stream" = "Opening stream…";

iina/Base.lproj/QuickSettingViewController.xib
@@ -94,7 +94,7 @@
                     <rect key="frame" x="0.0" y="823" width="120" height="48"/>
                     <buttonCell key="cell" type="square" title="VIDEO" bezelStyle="shadowlessSquare" alignment="center" imageScaling="proportionallyDown" inset="2" id="lQt-I0-jHO">
                         <behavior key="behavior" pushIn="YES" lightByBackground="YES" lightByGray="YES"/>
-                        <font key="font" metaFont="system"/>
+                        <font key="font" metaFont="systemBold"/>
                     </buttonCell>
                     <connections>
                         <action selector="tabBtnAction:" target="-2" id="T17-c3-4hp"/>
@@ -1417,7 +1417,7 @@
                                         <rect key="frame" x="0.0" y="0.0" width="360" height="823"/>
                                         <clipView key="contentView" drawsBackground="NO" copiesOnScroll="NO" id="epy-wp-Ja5">
                                             <rect key="frame" x="0.0" y="0.0" width="360" height="823"/>
-                                            <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
+                                            <autoresizingMask key="autoresizingMask"/>
                                             <subviews>
                                                 <customView translatesAutoresizingMaskIntoConstraints="NO" id="pm4-x9-WJ5" customClass="FlippedView" customModule="IINA" customModuleProvider="target">
                                                     <rect key="frame" x="0.0" y="-8" width="360" height="831"/>
@@ -1429,7 +1429,7 @@
                                                                     <rect key="frame" x="0.0" y="304" width="360" height="72"/>
                                                                     <clipView key="contentView" drawsBackground="NO" copiesOnScroll="NO" id="dJV-R0-O3M">
                                                                         <rect key="frame" x="0.0" y="0.0" width="360" height="72"/>
-                                                                        <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
+                                                                        <autoresizingMask key="autoresizingMask"/>
                                                                         <subviews>
                                                                             <tableView focusRingType="none" verticalHuggingPriority="750" allowsExpansionToolTips="YES" columnAutoresizingStyle="lastColumnOnly" columnResizing="NO" multipleSelection="NO" autosaveColumns="NO" rowSizeStyle="automatic" viewBased="YES" id="2vU-hm-gGB">
                                                                                 <rect key="frame" x="0.0" y="0.0" width="360" height="72"/>
@@ -1567,7 +1567,7 @@
                                                                     <rect key="frame" x="0.0" y="187" width="360" height="72"/>
                                                                     <clipView key="contentView" drawsBackground="NO" copiesOnScroll="NO" id="EKn-MX-Fao">
                                                                         <rect key="frame" x="0.0" y="0.0" width="360" height="72"/>
-                                                                        <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
+                                                                        <autoresizingMask key="autoresizingMask"/>
                                                                         <subviews>
                                                                             <tableView focusRingType="none" verticalHuggingPriority="750" allowsExpansionToolTips="YES" columnAutoresizingStyle="lastColumnOnly" columnResizing="NO" multipleSelection="NO" autosaveColumns="NO" rowSizeStyle="automatic" viewBased="YES" id="Jve-qX-Agy">
                                                                                 <rect key="frame" x="0.0" y="0.0" width="360" height="72"/>

iina/de.lproj/Localizable.strings
@@ -177,8 +177,6 @@
 "pl_menu.reveal_in_finder" = "Im Finder anzeigen";
 "pl_menu.title_multi" = "%d Dateien";
 "pl_menu.wrong_sub" = "Falscher Untertitel!";
-"playlist.chapter_cap" = "KAPITEL";
-"playlist.playlist_cap" = "WIEDERGABELISTE";
 "playlist.total_length" = "%@ insgesamt";
 "playlist.total_length_with_selected" = "%@ von %@ ausgewählt";
 "preference.advanced" = "Erweitert";

iina/es.lproj/Localizable.strings
@@ -177,8 +177,6 @@
 "pl_menu.reveal_in_finder" = "Mostrar en el Finder";
 "pl_menu.title_multi" = "%d Elementos";
 "pl_menu.wrong_sub" = "¡Subtítulo incorrecto!";
-"playlist.chapter_cap" = "CAPÍTULOS";
-"playlist.playlist_cap" = "LISTA DE REPRODUCCIÓN";
 "playlist.total_length" = "%@ en total";
 "playlist.total_length_with_selected" = "%@ de %@ seleccionados";
 "preference.advanced" = "Avanzado";

iina/fr.lproj/Localizable.strings
@@ -177,8 +177,6 @@
 "pl_menu.reveal_in_finder" = "Afficher dans le Finder";
 "pl_menu.title_multi" = "%d éléments";
 "pl_menu.wrong_sub" = "Mauvais sous-titre !";
-"playlist.chapter_cap" = "CHAPITRES";
-"playlist.playlist_cap" = "PLAYLIST";
 "playlist.total_length" = "%@ au total";
 "playlist.total_length_with_selected" = "%@ sur %@ sélectionnés";
 "preference.advanced" = "Avancé";

commit b0623738c2f057efd4728d054cc442c0040a310a

Fix regression introduced in 83a2f8c, #2360
Can't hide side bar when current tab is audio or subtitles

iina/PlaylistViewController.swift
@@ -208,12 +208,12 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
     switch tab {
     case .playlist:
       tabView.selectTabViewItem(at: 0)
-      Utility.setActive(playlistBtn, true)
-      Utility.setActive(chaptersBtn, false)
+      Utility.setBoldTitle(for: playlistBtn, true)
+      Utility.setBoldTitle(for: chaptersBtn, false)
     case .chapters:
       tabView.selectTabViewItem(at: 1)
-      Utility.setActive(chaptersBtn, true)
-      Utility.setActive(playlistBtn, false)
+      Utility.setBoldTitle(for: chaptersBtn, true)
+      Utility.setBoldTitle(for: playlistBtn, false)
     }
 
     currentTab = tab

iina/QuickSettingViewController.swift
@@ -8,6 +8,12 @@
 
 import Cocoa
 
+fileprivate extension QuickSettingViewController.TabViewType {
+  init(buttonTag: Int) {
+    self = [.video, .audio, .sub][at: buttonTag] ?? .video
+  }
+}
+
 class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate, SidebarViewController {
 
   override var nibName: NSNib.Name {
@@ -375,8 +381,8 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
 
   @IBAction func tabBtnAction(_ sender: NSButton) {
     tabView.selectTabViewItem(at: sender.tag)
-    [videoTabBtn, audioTabBtn, subTabBtn].forEach { Utility.setActive($0, false) }
-    Utility.setActive(sender, true)
+    [videoTabBtn, audioTabBtn, subTabBtn].forEach { Utility.setBoldTitle(for: $0, $0 == sender) }
+    currentTab = .init(buttonTag: sender.tag)
     reload()
   }
 

iina/Utility.swift
@@ -384,7 +384,7 @@ class Utility {
 
   // MARK: - Util functions
 
-  static func setActive(_ button: NSButton, _ active: Bool) {
+  static func setBoldTitle(for button: NSButton, _ active: Bool) {
     button.attributedTitle = NSAttributedString(string: button.title,
                                                 attributes: FontAttributes(font: active ? .systemBold : .system, size: .system, align: .center).value)
   }
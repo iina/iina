9c9c390aa899a2e0262b936511fd92c291b971ff
Saagar Jha <saagar@saagarjha.com>
2019-04-02 00:58:50 -0700

    Replace use of std::unique_ptr with DEFER macro

--- iina/FFmpegController.mm
+++ iina/FFmpegController.mm
@@ -26,60 +26,22 @@ extern "C" {
 #define DEFAULT_THUMBNAIL_COUNT 100
 #define DEFAULT_THUMBNAIL_WIDTH 240.0
 
-struct AVFormatContextCloser {
-  void operator()(AVFormatContext *formatContext) {
-    avformat_close_input(&formatContext);
-  }
-};
-
-struct AVCodecContextDeleter {
-  virtual void operator()(AVCodecContext *codecContext) {
-    avcodec_free_context(&codecContext);
-  }
-};
-
-struct AVCodecContextDeleterCloser : AVCodecContextDeleter {
-  void operator()(AVCodecContext *codecContext) override {
-    AVCodecContextDeleter()(codecContext);
-    avcodec_close(codecContext);
-  }
-};
-
-struct AVFrameDeleter {
-  void operator()(AVFrame *frame) {
-    av_frame_free(&frame);
-  }
-};
-
-struct AVDeleter {
-  void operator()(uint8_t *pointer) {
-    av_free(pointer);
-  }
-};
-
-struct SwsContextDeleter {
-  void operator()(SwsContext *swsContext) {
-    sws_freeContext(swsContext);
-  }
-};
-
-struct AVPacketUnrefer {
-  void operator()(AVPacket *packet) {
-    av_packet_unref(packet);
-  }
-};
+#define __ACTUALLY_CONCATENATE(a, b) a##b
+#define __CONCATENATE(a, b) __ACTUALLY_CONCATENATE(a, b)
+#define DEFER(cleanup) std::shared_ptr<void> __CONCATENATE(__defer_, __LINE__)(nullptr, cleanup)
 
 @implementation FFmpegController (Bridge)
 
 - (BOOL)synchronouslyGenerateThumbnailsForFileAtPath:(NSString *)path {
-  std::unique_ptr<AVFormatContext, AVFormatContextCloser> formatContext;
-  auto raw_formatContext = formatContext.release();
-  if (avformat_open_input(&raw_formatContext, path.fileSystemRepresentation, nullptr, nullptr) < 0) {
+  AVFormatContext *formatContext = NULL;
+  if (avformat_open_input(&formatContext, path.fileSystemRepresentation, nullptr, nullptr) < 0) {
     return NO;
   }
-  formatContext.reset(raw_formatContext);
+  DEFER([&](...) {
+    avformat_close_input(&formatContext);
+  });
 
-  if (avformat_find_stream_info(formatContext.get(), nullptr) < 0) {
+  if (avformat_find_stream_info(formatContext, nullptr) < 0) {
     return NO;
   }
   auto videoStreamIndex = std::find_if(formatContext->streams, formatContext->streams + formatContext->nb_streams, [](const AVStream *stream) {
@@ -96,29 +58,40 @@ struct AVPacketUnrefer {
 
   auto codec = avcodec_find_decoder(videoStream->codecpar->codec_id);
 
-  std::unique_ptr<AVCodecContext, AVCodecContextDeleter> codecContext(avcodec_alloc_context3(codec));
+  AVCodecContext *codecContext = avcodec_alloc_context3(codec);
+  DEFER([&](...) {
+    avcodec_free_context(&codecContext);
+  });
   if (!codecContext) {
     return NO;
   }
 
-  if (avcodec_parameters_to_context(codecContext.get(), videoStream->codecpar) < 0) {
+  if (avcodec_parameters_to_context(codecContext, videoStream->codecpar) < 0) {
     return NO;
   }
   codecContext->time_base = videoStream->time_base;
 
-  if (avcodec_open2(codecContext.get(), codec, nullptr) < 0) {
+  if (avcodec_open2(codecContext, codec, nullptr) < 0) {
     return NO;
   }
-  codecContext = std::unique_ptr<AVCodecContext, AVCodecContextDeleterCloser>(codecContext.release());
+  DEFER([&](...) {
+    avcodec_close(codecContext);
+  });
 
-  std::unique_ptr<AVFrame, AVFrameDeleter> frame(av_frame_alloc());
+  AVFrame *frame = av_frame_alloc();
   if (!frame) {
     return NO;
   }
-  std::unique_ptr<AVFrame, AVFrameDeleter> frameRGB(av_frame_alloc());
+  DEFER([&](...) {
+    av_frame_free(&frame);
+  });
+  AVFrame *frameRGB = av_frame_alloc();
   if (!frameRGB) {
     return NO;
   }
+  DEFER([&](...) {
+    av_frame_free(&frameRGB);
+  });
 
   frameRGB->width = DEFAULT_THUMBNAIL_WIDTH;
   frameRGB->height = DEFAULT_THUMBNAIL_WIDTH * codecContext->height / codecContext->width;
@@ -132,11 +105,14 @@ struct AVPacketUnrefer {
   if (size < 0) {
     return NO;
   }
-  std::unique_ptr<uint8_t, AVDeleter> frameRGBBuffer(static_cast<uint8_t *>(av_malloc(size)));
+  uint8_t *frameRGBBuffer = static_cast<uint8_t *>(av_malloc(size));
+  DEFER([&](...) {
+    av_free(frameRGBBuffer);
+  });
   if (av_image_fill_arrays(
           frameRGB->data,
           frameRGB->linesize,
-          frameRGBBuffer.get(),
+          frameRGBBuffer,
           AV_PIX_FMT_RGBA,
           frameRGB->width,
           frameRGB->height,
@@ -144,7 +120,7 @@ struct AVPacketUnrefer {
     return NO;
   }
 
-  std::unique_ptr<SwsContext, SwsContextDeleter> swsContext(sws_getContext(
+  SwsContext* swsContext = sws_getContext(
       codecContext->width,
       codecContext->height,
       codecContext->pix_fmt,
@@ -153,30 +129,35 @@ struct AVPacketUnrefer {
       AV_PIX_FMT_RGBA, SWS_BILINEAR,
       nullptr,
       nullptr,
-      nullptr));
+      nullptr);
   if (!swsContext) {
     return NO;
   }
+  DEFER([&](...) {
+    sws_freeContext(swsContext);
+  });
 
   auto interval = 1.0 * av_rescale_q(formatContext->duration, AV_TIME_BASE_Q, videoStream->time_base) / DEFAULT_THUMBNAIL_COUNT;
 
   for (auto progress = 0; progress < DEFAULT_THUMBNAIL_COUNT; ++progress) {
-    avcodec_flush_buffers(codecContext.get());
+    avcodec_flush_buffers(codecContext);
     av_seek_frame(
-        formatContext.get(),
+        formatContext,
         static_cast<int>(videoStreamIndex - formatContext->streams),
         videoStream->start_time + interval * progress,
         AVSEEK_FLAG_BACKWARD);
-    AVPacket raw_packet;
-    while (!av_read_frame(formatContext.get(), &raw_packet)) {
-      std::unique_ptr<AVPacket, AVPacketUnrefer> packet(&raw_packet);
-      if (packet->stream_index == videoStreamIndex - formatContext->streams) {
-        if (avcodec_send_packet(codecContext.get(), packet.get()) != 0) {
+    AVPacket packet;
+    while (!av_read_frame(formatContext, &packet)) {
+      DEFER([&](...) {
+        av_packet_unref(&packet);
+      });
+      if (packet.stream_index == videoStreamIndex - formatContext->streams) {
+        if (avcodec_send_packet(codecContext, &packet) != 0) {
           break;
         }
       }
 
-      auto result = avcodec_receive_frame(codecContext.get(), frame.get());
+      auto result = avcodec_receive_frame(codecContext, frame);
       if (result == AVERROR(EAGAIN)) {
         continue;
       } else if (result < 0) {
@@ -188,7 +169,7 @@ struct AVPacketUnrefer {
       }
 
       if (sws_scale(
-              swsContext.get(),
+              swsContext,
               frame->data,
               frame->linesize,
               0,
@@ -206,14 +187,15 @@ struct AVPacketUnrefer {
 }
 
 + (double)videoDurationForFileAtPath:(NSString *)path {
-  std::unique_ptr<AVFormatContext, AVFormatContextCloser> formatContext;
-  auto raw_formatContext = formatContext.release();
-  if (avformat_open_input(&raw_formatContext, path.fileSystemRepresentation, nullptr, nullptr) < 0) {
+  AVFormatContext *formatContext;
+  if (avformat_open_input(&formatContext, path.fileSystemRepresentation, nullptr, nullptr) < 0) {
     return -1;
   }
-  formatContext.reset(raw_formatContext);
+  DEFER([&](...){
+    avformat_close_input(&formatContext);
+  });
   if (formatContext->duration < 0) {
-    if (avformat_find_stream_info(formatContext.get(), nullptr) < 0) {
+    if (avformat_find_stream_info(formatContext, nullptr) < 0) {
       return -1;
     }
   }

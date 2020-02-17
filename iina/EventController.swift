ddaf226ee3f49fb0646d6b15d0948d2e2b028d5e
Yuze Jiang <fortissimoX@gmail.com>
2020-02-17 15:34:13 -0600

    Plugin: Add more events
    Will emit those window-related events after #2772 is merged

--- iina/EventController.swift
+++ iina/EventController.swift
@@ -26,10 +26,39 @@ class EventController {
     init?(rawValue: RawValue) { self.rawValue = rawValue }
 
     // IINA events
+    
+    // Window related
     static let windowLoaded = Name("iina.window-loaded")
     static let windowMoved = Name("iina.window-moved")
     static let windowResized = Name("iina.window-resized")
+    static let windowFullscreenChanged = Name("iina.window.fs-changed")
+    static let windowChangedScreen = Name("iina.window-changed-screen")
+    static let windowMiniaturized = Name("iina.window-miniaturized")
+    static let windowDeminiaturized = Name("iina.window-deminiaturized")
+    
+    static let windowBecameMain = Name("iina.window-became-main")
+    static let windowResignedMain = Name("iina.window-resigned-main")
+    static let windowWillClose = Name("iina.window-will-close")
+    static let windowDidClose = Name("iina.window-did-close") // might shut down the whole app
+
+    static let musicModeChanged = Name("iina.music-mode-changed")
     static let pipChanged = Name("iina.pip.changed")
+
+    // File related
+    static let fileLoaded = Name("iina.file-loaded")
+    static let fileStarted = Name("iina.file-started")
+    
+    // Playlist related
+    static let fileLoopChanged = Name("iina.file-loop-changed")
+    static let playlistLoopChanged = Name("iina.playlist-loop-changed")
+    
+    // Playback related
+    static let playbackPaused = Name("iina.playback.paused")
+    static let playbackResumed = Name("iina.playback.resumed")
+    
+    // Others
+    static let mpvInitialized = Name("iina.mpv-inititalized")
+    static let thumbnailsReady = Name("iina.thumbnails-ready")
   }
 
   var listeners: [Name: [String: EventCallable]] = [:]

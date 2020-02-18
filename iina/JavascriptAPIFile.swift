6d020895a6a4242c525a3f96a76f14f44ed18c24
Yuze Jiang <fortissimoX@gmail.com>
2020-02-18 17:28:45 -0600

    Plugin: Add file I/O API, can only read/write to .data folder

--- iina/JavascriptAPIFile.swift
+++ iina/JavascriptAPIFile.swift
@@ -11,12 +11,25 @@ import JavaScriptCore
 
 @objc protocol JavascriptAPIFileExportable: JSExport {
   func getFileNamesFromCurrentDir() -> JSValue
+  func write(_ fileName: String, _ content: String)
+  func read(_ fileName: String) -> String
 }
 
 class JavascriptAPIFile: JavascriptAPI, JavascriptAPIFileExportable {
 
   private let fileManager = FileManager.default
-  
+
+  private lazy var dataURL: URL = {
+    let url = Utility.pluginsURL.appendingPathComponent(".data", isDirectory: true)
+      .appendingPathComponent(pluginInstance.plugin.identifier, isDirectory: true)
+    Utility.createDirIfNotExist(url: url)
+    return url
+  }()
+
+  private lazy var dataFolderPath: String = {
+    return dataURL.path + "/"
+  }()
+
   static func currentDir(_ player: PlayerCore) -> URL? {
     let folder = player.info.currentURL?.deletingLastPathComponent()
     return folder?.isFileURL ?? false ? folder : nil
@@ -32,5 +45,25 @@ class JavascriptAPIFile: JavascriptAPI, JavascriptAPIFileExportable {
       return JSValue(newErrorFromMessage: "Error when fetching contents of directory.", in: context)
     }
     return JSValue(object: urls, in: context)
+  }
+
+  @objc func write(_ fileName: String, _ content: String) {
+    do {
+      try content.write(toFile: dataFolderPath + fileName, atomically: true, encoding: .utf8)
+    } catch {
+      throwError(withMessage: "Error when write to file \(fileName)")
+    }
+  }
+
+  @objc func read(_ fileName: String) -> String {
+    guard let streamReader = StreamReader(path: dataFolderPath + fileName) else {
+      throwError(withMessage: "Cannot open the file.")
+      return ""
+    }
+    var ret = ""
+    while let line = streamReader.nextLine() {
+      ret += line
+    }
+    return ret
   }
 }

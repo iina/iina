058fa27b3be31b17f01e03f5f62d7bdcd2eb3e99
Yuze Jiang <fortissimoX@gmail.com>
2020-02-20 16:15:56 -0600

    Plugin: (APIFile) check permissions

--- iina/JavascriptAPIFile.swift
+++ iina/JavascriptAPIFile.swift
@@ -36,37 +36,43 @@ class JavascriptAPIFile: JavascriptAPI, JavascriptAPIFileExportable {
   }
 
   @objc func getFileNamesFromCurrentDir() -> JSValue? {
-    guard let folder = JavascriptAPIFile.currentDir(player) else {
-      throwError(withMessage: "Cannot get current folder.")
-      return nil
+    return whenPermitted(to: .filenameCurrentdir) {
+      guard let folder = JavascriptAPIFile.currentDir(player) else {
+        throwError(withMessage: "Cannot get current folder.")
+        return nil
+      }
+      let urls: [String]
+      do {
+        urls = try fileManager.contentsOfDirectory(atPath: folder.path)
+      } catch {
+        throwError(withMessage: "Error when fetching contents of directory.")
+        return nil
+      }
+      return JSValue(object: urls, in: context)
     }
-    let urls: [String]
-    do {
-      urls = try fileManager.contentsOfDirectory(atPath: folder.path)
-    } catch {
-      throwError(withMessage: "Error when fetching contents of directory.")
-      return nil
-    }
-    return JSValue(object: urls, in: context)
   }
 
   @objc func write(_ fileName: String, _ content: String) {
-    do {
-      try content.write(toFile: dataFolderPath + fileName, atomically: true, encoding: .utf8)
-    } catch {
-      throwError(withMessage: "Error when write to file \(fileName)")
+    whenPermitted(to: .fileIO) {
+      do {
+        try content.write(toFile: dataFolderPath + fileName, atomically: true, encoding: .utf8)
+      } catch {
+        throwError(withMessage: "Error when write to file \(fileName)")
+      }
     }
   }
 
   @objc func read(_ fileName: String) -> String? {
-    guard let streamReader = StreamReader(path: dataFolderPath + fileName) else {
-      throwError(withMessage: "Cannot open the file.")
-      return nil
+    return whenPermitted(to: .fileIO) {
+      guard let streamReader = StreamReader(path: dataFolderPath + fileName) else {
+        throwError(withMessage: "Cannot open the file.")
+        return nil
+      }
+      var ret = ""
+      while let line = streamReader.nextLine() {
+        ret += line
+      }
+      return ret
     }
-    var ret = ""
-    while let line = streamReader.nextLine() {
-      ret += line
-    }
-    return ret
   }
 }

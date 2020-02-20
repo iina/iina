b52661215b943dfb0e61492ce2e2a54fb5bef7e9
Yuze Jiang <fortissimoX@gmail.com>
2020-02-20 16:47:01 -0600

    Plugin: Add playlist APIs

--- iina/MainWindowController.swift
+++ iina/MainWindowController.swift
@@ -1540,10 +1540,6 @@ class MainWindowController: PlayerWindowController {
     player.events.emit(.windowMoved, data: window.frame)
   }
 
-  func windowDidChangeScreen(_ notification: Notification) {
-    videoView.updateDisplayLink()
-  }
-
   func windowDidBecomeKey(_ notification: Notification) {
     window!.makeFirstResponder(window!)
     if Preference.bool(for: .pauseWhenInactive) && isPausedDueToInactive {

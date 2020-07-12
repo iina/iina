40e68c73be00bc908b4c326eb6f193e322807e0a
Collider LI <lhc199652@gmail.com>
2020-07-13 01:27:50 +0800

    Plugin: download online subtitles

--- iina/JavascriptPluginSubtitle.swift
+++ iina/JavascriptPluginSubtitle.swift
@@ -13,19 +13,40 @@ import PromiseKit
 class JSPluginSub {
   class Subtitle: OnlineSubtitle {
     var item: JavascriptPluginSubtitleItem
+    var api: JavascriptAPISubtitle
 
-    init(index: Int, item: JavascriptPluginSubtitleItem) {
+    init(index: Int, item: JavascriptPluginSubtitleItem, api: JavascriptAPISubtitle) {
       self.item = item
+      self.api = api
       super.init(index: index)
     }
 
     override func getDescription() -> (name: String, left: String, right: String) {
-      let data = item.data;
+      let data = item.desc;
       return (
-        data.objectForKeyedSubscript("$name")?.toString() ?? "",
-        data.objectForKeyedSubscript("$left")?.toString() ?? "",
-        data.objectForKeyedSubscript("$right")?.toString() ?? ""
+        data?.objectForKeyedSubscript("name")?.toString() ?? "",
+        data?.objectForKeyedSubscript("left")?.toString() ?? "",
+        data?.objectForKeyedSubscript("right")?.toString() ?? ""
       )
+    }
+
+    override func download() -> Promise<[URL]> {
+      guard let download = item.download else { return .value([]) }
+      return Promise { resolver in
+        let completed: @convention(block) (Any) -> Void = { urls in
+          guard let urls = urls as? [String] else {
+            resolver.reject(Error.pluginError("provider.download should return an array of strings."))
+            return
+          }
+          resolver.fulfill(urls.compactMap{ self.api.parsePath($0).path }.map(URL.init(fileURLWithPath:)))
+        }
+        let failed: @convention(block) (String) -> Void = {
+          resolver.reject(Error.pluginError($0))
+        }
+        let c = JSValue(object: completed, in: download.context)!
+        let f = JSValue(object: failed, in: download.context)!
+        download.call(withArguments: [c, f])
+      }
     }
   }
 
@@ -56,8 +77,12 @@ class JSPluginSub {
 
     func search(api: JavascriptAPISubtitle, id: String) -> Promise<[Subtitle]> {
       return Promise { resolver in
-        let completed: @convention(block) ([JavascriptPluginSubtitleItem]) -> Void = { subs in
-          resolver.fulfill(subs.enumerated().map{ (i, v) in Subtitle(index: i, item: v) })
+        let completed: @convention(block) (Any) -> Void = { subs in
+          guard let subs = subs as? [JavascriptPluginSubtitleItem] else {
+            resolver.reject(Error.pluginError("provider.search should return an array of subtitle items."))
+            return
+          }
+          resolver.fulfill(subs.enumerated().map{ (i, v) in Subtitle(index: i, item: v, api: api) })
         }
         let failed: @convention(block) (String) -> Void = {
           resolver.reject(Error.pluginError($0))

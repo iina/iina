iina/iina

commit e9b1b688473171ca3acd6aa7f754eab4810e0280

Monkey patch to fix window not responding to close/minimize actions while UI is hidden

iina/MainWindowController.swift
@@ -1513,6 +1513,7 @@ class MainWindowController: PlayerWindowController {
         self.fadeableViews.forEach { (v) in
           v.isHidden = true
         }
+        self.standardWindowButtons.forEach { $0.alphaValue = 1e-100 }
         self.animationState = .hidden
       }
     }

commit 30f1675b2c7179876c4a183d7d9ca6499af4350a

Fix standard buttons not visible in full screen
caused by e9b1b68

iina/MainWindowController.swift
@@ -1511,9 +1511,12 @@ class MainWindowController: PlayerWindowController {
       // if no interrupt then hide animation
       if self.animationState == .willHide {
         self.fadeableViews.forEach { (v) in
-          v.isHidden = true
+          if let btn = v as? NSButton, self.standardWindowButtons.contains(btn) {
+            v.alphaValue = 1e-100
+          } else {
+            v.isHidden = true
+          }
         }
-        self.standardWindowButtons.forEach { $0.alphaValue = 1e-100 }
         self.animationState = .hidden
       }
     }
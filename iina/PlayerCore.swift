eddd09f4cdb6ca7f5c58ad6e961770409adfb4a0
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2022-01-02 19:07:38 +0800

    fix: playerCore emit fileLoaded param
    The fileLoad event callback param here is NSURL, in plugin js, this object cannot use to get any info.
    And the NSURL not corresponding d.ts ( says url is a string) https://github.com/iina/iina-plugin-definition/blob/master/iina/index.d.ts#L168

--- iina/PlayerCore.swift
+++ iina/PlayerCore.swift
@@ -1211,7 +1211,7 @@ class PlayerCore: NSObject {
 
     }
     postNotification(.iinaFileLoaded)
-    events.emit(.fileLoaded, data: info.currentURL ?? "")
+    events.emit(.fileLoaded, data: info.currentURL?.absoluteString ?? "")
   }
 
   func playbackRestarted() {

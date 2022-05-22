ec7d89f5acd8852cdd3fdb1ae78b85e968e7a4a6
Anas Saeed <saeedanas396@gmail.com>
2022-05-22 02:20:21 -0700

    Added Search Button in PlaylistViewController and implemented open and closing functionality

--- iina/PlaylistSearchViewController.swift
+++ iina/PlaylistSearchViewController.swift
@@ -11,4 +11,83 @@ import Cocoa
 
 class PlaylistSearchViewController: NSWindowController {
   
+  override var windowNibName: NSNib.Name {
+    return NSNib.Name("PlaylistSearchViewController")
+  }
+  
+  weak var playlistViewController: PlaylistViewController! {
+    didSet {
+      self.player = playlistViewController.player
+    }
+  }
+  
+  weak var player: PlayerCore!
+  
+  private var clickMonitor: Any?
+  private var isOpen = false
+  
+  // MARK: Outlets
+  @IBOutlet weak var inputField: NSTextField!
+  @IBOutlet weak var clearBtn: NSButton!
+  @IBOutlet weak var searchResultsTableView: NSTableView!
+  @IBOutlet weak var inputBorderBottom: NSBox!
+  
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    
+    // Remove window titlebar and buttons
+    window?.isMovableByWindowBackground = true
+    window?.titlebarAppearsTransparent = true
+    window?.titleVisibility = .hidden
+    ([.closeButton, .miniaturizeButton, .zoomButton] as [NSWindow.ButtonType]).forEach {
+      window?.standardWindowButton($0)?.isHidden = true
+    }
+  }
+  
+  // MARK: Opening and Closing Window
+  func openSearchWindow() {
+    if isOpen {
+      return
+    }
+    isOpen = true
+    
+    addClickMonitor()
+    showWindow(nil)
+  }
+  
+  func hideSearchWindow() {
+    if !isOpen {
+      return
+    }
+    isOpen = false
+    
+    window?.close()
+    removeClickMonitor()
+  }
+  
+  @objc func cancel(_ sender: Any?) {
+    hideSearchWindow()
+  }
+  
+  // MARK: Click Events
+  /**
+   Creates a monitor for outside clicks. If user clicks outside the search window, the window will be hidden
+   */
+  func addClickMonitor() {
+    clickMonitor = NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.leftMouseDown) {
+      (event) -> NSEvent? in
+      if !(self.window?.windowNumber == event.windowNumber) {
+        self.hideSearchWindow()
+      }
+      return event
+    }
+  }
+  
+  func removeClickMonitor() {
+    if clickMonitor != nil {
+      NSEvent.removeMonitor(clickMonitor!)
+      clickMonitor = nil
+    }
+  }
+  
 }

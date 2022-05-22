39e7906faeade61981902e7e41ea577693d4083c
Anas Saeed <saeedanas396@gmail.com>
2022-05-22 01:17:17 -0700

    Added basic PlaylistSearchViewWindow Layout

---
+++ iina/PlaylistSearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlaylistSearchViewController.swift
+//  iina
+//
+//  Created by Anas Saeed on 5/22/22.
+//  Copyright © 2022 lhc. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class PlaylistSearchViewController: NSWindowController {
+  
+}

iina/iina

commit 60b944f63c526cf377b95a063d751c592a949ef4

Plugin: Sidebar

iina/MainWindowController.swift
@@ -2544,38 +2544,60 @@ class MainWindowController: PlayerWindowController {
     setWindowFloatingOnTop(!isOntop)
   }
 
-  /// Legacy IBAction, but still in use.
-  func settingsButtonAction(_ sender: AnyObject) {
-    if sidebarAnimationState == .willShow || sidebarAnimationState == .willHide {
-      return  // do not interrupt other actions while it is animating
+  func showSettingsSidebar(tab: QuickSettingViewController.TabViewType? = nil, force: Bool = false, hideIfAlreadyShown: Bool = true) {
+    if !force && sidebarAnimationState == .willShow || sidebarAnimationState == .willHide {
+      return  // do not interrput other actions while it is animating
     }
     let view = quickSettingView
     switch sideBarStatus {
     case .hidden:
+      if let tab = tab {
+        view.pleaseSwitchToTab(tab)
+      }
       showSideBar(viewController: view, type: .settings)
     case .playlist:
+      if let tab = tab {
+        view.pleaseSwitchToTab(tab)
+      }
       hideSideBar {
         self.showSideBar(viewController: view, type: .settings)
       }
     case .settings:
-      hideSideBar()
+      if view.currentTab == tab {
+        if hideIfAlreadyShown {
+          hideSideBar()
+        }
+      } else if let tab = tab {
+        view.pleaseSwitchToTab(tab)
+      }
     }
   }
 
-  /// Legacy IBAction, but still in use.
-  func playlistButtonAction(_ sender: AnyObject) {
-    if sidebarAnimationState == .willShow || sidebarAnimationState == .willHide {
-      return  // do not interrupt other actions while it is animating
+  func showPlaylistSidebar(tab: PlaylistViewController.TabViewType? = nil, force: Bool = false, hideIfAlreadyShown: Bool = true) {
+    if !force && sidebarAnimationState == .willShow || sidebarAnimationState == .willHide {
+      return  // do not interrput other actions while it is animating
     }
     let view = playlistView
     switch sideBarStatus {
     case .hidden:
-      showSideBar(viewController: view, type: .playlist)
+      if let tab = tab {
+        view.pleaseSwitchToTab(tab)
+      }
+      showSideBar(viewController: view, type: .settings)
     case .playlist:
-      hideSideBar()
-    case .settings:
+      if let tab = tab {
+        view.pleaseSwitchToTab(tab)
+      }
       hideSideBar {
-        self.showSideBar(viewController: view, type: .playlist)
+        self.showSideBar(viewController: view, type: .settings)
+      }
+    case .settings:
+      if view.currentTab == tab {
+        if hideIfAlreadyShown {
+          hideSideBar()
+        }
+      } else if let tab = tab {
+        view.pleaseSwitchToTab(tab)
       }
     }
   }
@@ -2611,9 +2633,9 @@ class MainWindowController: PlayerWindowController {
         }
       }
     case .playlist:
-      playlistButtonAction(sender)
+      showPlaylistSidebar()
     case .settings:
-      settingsButtonAction(sender)
+      showSettingsSidebar()
     case .subTrack:
       quickSettingView.showSubChooseMenu(forView: sender, showLoadedSubs: true)
     }

iina.xcodeproj/project.pbxproj
@@ -270,6 +270,10 @@
 		E38B3214214FB9EA000F6D27 /* PrefPluginPermissionView.xib in Resources */ = {isa = PBXBuildFile; fileRef = E38B3212214FB9EA000F6D27 /* PrefPluginPermissionView.xib */; };
 		E38B3216214FF700000F6D27 /* EventController.swift in Sources */ = {isa = PBXBuildFile; fileRef = E38B3215214FF700000F6D27 /* EventController.swift */; };
 		E38BD4AF20054BD9007635FC /* MainWindow.swift in Sources */ = {isa = PBXBuildFile; fileRef = E38BD4AE20054BD9007635FC /* MainWindow.swift */; };
+		E3958565253133E80096811F /* SidebarTabView.swift in Sources */ = {isa = PBXBuildFile; fileRef = E3958563253133E80096811F /* SidebarTabView.swift */; };
+		E3958566253133E80096811F /* SidebarTabView.xib in Resources */ = {isa = PBXBuildFile; fileRef = E3958564253133E80096811F /* SidebarTabView.xib */; };
+		E3958586253309C90096811F /* PluginSidebarView.swift in Sources */ = {isa = PBXBuildFile; fileRef = E3958585253309C90096811F /* PluginSidebarView.swift */; };
+		E395858A253314400096811F /* JavascriptAPISidebarView.swift in Sources */ = {isa = PBXBuildFile; fileRef = E39585892533143F0096811F /* JavascriptAPISidebarView.swift */; };
 		E3965E1A2487C99900607EB4 /* JavascriptPluginSubtitle.swift in Sources */ = {isa = PBXBuildFile; fileRef = E3965E192487C99900607EB4 /* JavascriptPluginSubtitle.swift */; };
 		E39A11FD240F176E00A67F9F /* StringEncodingName.swift in Sources */ = {isa = PBXBuildFile; fileRef = E39A11FC240F176E00A67F9F /* StringEncodingName.swift */; };
 		E3B3DD5720FDB8A600325184 /* PreferenceWindowController.xib in Resources */ = {isa = PBXBuildFile; fileRef = E3B3DD5920FDB8A600325184 /* PreferenceWindowController.xib */; };
@@ -1417,6 +1421,10 @@
 		E38B3215214FF700000F6D27 /* EventController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EventController.swift; sourceTree = "<group>"; };
 		E38BD4AE20054BD9007635FC /* MainWindow.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MainWindow.swift; sourceTree = "<group>"; };
 		E393669923BDD1850070D626 /* IINA.entitlements */ = {isa = PBXFileReference; lastKnownFileType = text.plist.entitlements; path = IINA.entitlements; sourceTree = "<group>"; };
+		E3958563253133E80096811F /* SidebarTabView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SidebarTabView.swift; sourceTree = "<group>"; };
+		E3958564253133E80096811F /* SidebarTabView.xib */ = {isa = PBXFileReference; lastKnownFileType = file.xib; path = SidebarTabView.xib; sourceTree = "<group>"; };
+		E3958585253309C90096811F /* PluginSidebarView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PluginSidebarView.swift; sourceTree = "<group>"; };
+		E39585892533143F0096811F /* JavascriptAPISidebarView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = JavascriptAPISidebarView.swift; sourceTree = "<group>"; };
 		E3965E192487C99900607EB4 /* JavascriptPluginSubtitle.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = JavascriptPluginSubtitle.swift; sourceTree = "<group>"; };
 		E39A11FC240F176E00A67F9F /* StringEncodingName.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = StringEncodingName.swift; sourceTree = "<group>"; };
 		E3B3DD5820FDB8A600325184 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.xib; name = Base; path = Base.lproj/PreferenceWindowController.xib; sourceTree = "<group>"; };
@@ -2099,6 +2107,8 @@
 				E34C0C3024934D7B0065A0C6 /* Switch.swift */,
 				E32712AE24EAA14500359DAB /* ScreenshootOSDView.swift */,
 				E32712B424F2B8CA00359DAB /* ScreenshootOSDView.xib */,
+				E3958563253133E80096811F /* SidebarTabView.swift */,
+				E3958564253133E80096811F /* SidebarTabView.xib */,
 			);
 			name = Accessories;
 			sourceTree = "<group>";
@@ -2229,6 +2239,7 @@
 				E3366AF9224A039500EF3C32 /* JavascriptAPIPreferences.swift */,
 				E337D5E5241EF60400B5729A /* JavascriptAPISubtitle.swift */,
 				E3839C5824FFF817007D0AB4 /* JavascriptAPIStandaloneWindow.swift */,
+				E39585892533143F0096811F /* JavascriptAPISidebarView.swift */,
 				E34EAA82251A36CE00057F27 /* JavascriptAPIFile.swift */,
 			);
 			name = API;
@@ -2244,6 +2255,7 @@
 				E337D5E12411B64900B5729A /* JavascriptPolyfill.swift */,
 				E30D2EBC21F5FD2600E1FF0D /* PluginOverlayView.swift */,
 				E3839C5624FFF201007D0AB4 /* PluginStandaloneWindow.swift */,
+				E3958585253309C90096811F /* PluginSidebarView.swift */,
 				E3965E192487C99900607EB4 /* JavascriptPluginSubtitle.swift */,
 				E32693F824BA138000BF7ADC /* JavascriptPluginSubtitleItem.swift */,
 			);
@@ -2447,6 +2459,7 @@
 				E34A0E012053F93A00B50097 /* PrefOSCToolbarSettingsSheetController.xib in Resources */,
 				E34E909A24F821280073BF04 /* GuideWindowController.xib in Resources */,
 				84AF03D21E67492C003E3753 /* Contribution.rtf in Resources */,
+				E3958566253133E80096811F /* SidebarTabView.xib in Resources */,
 				8400D5D21E1AB312006785F5 /* InspectorWindowController.xib in Resources */,
 				8400D5CC1E1AB2FF006785F5 /* PlaylistViewController.xib in Resources */,
 				8400D5ED1E1AB33F006785F5 /* PrefAdvancedViewController.xib in Resources */,
@@ -2523,6 +2536,7 @@
 				84BEEC421DFEE46200F945CA /* StreamReader.swift in Sources */,
 				E3CAD4AF2146DB47000B373A /* JavascriptAPI.swift in Sources */,
 				84C644601E92BA2700B1410B /* TimeLabelOverflowedView.swift in Sources */,
+				E3958565253133E80096811F /* SidebarTabView.swift in Sources */,
 				E3C12F6B201F281F00297964 /* FirstRunManager.swift in Sources */,
 				84EB1F071D2F5E76004FA5A1 /* Utility.swift in Sources */,
 				84D0FB7E1F5E519300C6A6A7 /* FreeSelectingViewController.swift in Sources */,
@@ -2615,11 +2629,13 @@
 				84C8D58F1D794CE600D98A0E /* MPVFilter.swift in Sources */,
 				845404AA1E4396F500B02B12 /* ScriptLoader.swift in Sources */,
 				84A0BAA11D2FAE7600BC8DA1 /* VideoView.swift in Sources */,
+				E395858A253314400096811F /* JavascriptAPISidebarView.swift in Sources */,
 				E33BA5C4204315740069A0F6 /* AssrtSubtitle.swift in Sources */,
 				E32693F924BA138000BF7ADC /* JavascriptPluginSubtitleItem.swift in Sources */,
 				844DE1BF1D3E2B9900272589 /* MPVEvent.swift in Sources */,
 				845040471E0B0F500079C194 /* CropSettingsViewController.swift in Sources */,
 				E33836852026223800ABC812 /* PrefOSCToolbarSettingsSheetController.swift in Sources */,
+				E3958586253309C90096811F /* PluginSidebarView.swift in Sources */,
 				E301EFDA21312AB300BC8588 /* KeychainAccess.swift in Sources */,
 				8434BAA71D5DF2DA003BECF2 /* Extensions.swift in Sources */,
 				845404AC1E43980900B02B12 /* LuaScript.swift in Sources */,

iina/JavascriptAPICore.swift
@@ -22,6 +22,7 @@ import JavaScriptCore
   func playChapter(index: Int)
   func getHistory() -> Any
   func getRecentDocuments() -> Any
+  func getVersion() -> Any
 }
 
 class JavascriptAPICore: JavascriptAPI, JavascriptAPICoreExportable {
@@ -103,6 +104,15 @@ class JavascriptAPICore: JavascriptAPI, JavascriptAPICoreExportable {
         "url": $0.absoluteString
       ]
     }
+  }
+
+  func getVersion() -> Any {
+    let (iinaVersion, build) = Utility.iinaVersion()
+    return [
+      "iina": iinaVersion,
+      "build": build,
+      "mpv": PlayerCore.first.mpv.mpvVersion
+    ]
   }
 }
 
@@ -206,6 +216,8 @@ fileprivate class WindowAPI: JavascriptAPI, CoreSubAPIExportable {
       return window.isOntop
     case "visible":
       return window.window!.occlusionState == .visible
+    case "sidebar":
+      return window.sideBarStatus == .settings ? window.quickSettingView.currentTab.name : NSNull()
     case "screens":
       let current = window.window!.screen!
       let main = NSScreen.main
@@ -246,6 +258,18 @@ fileprivate class WindowAPI: JavascriptAPI, CoreSubAPIExportable {
     case "ontop":
       guard let val = value as? Bool else { return }
       window.setWindowFloatingOnTop(val)
+    case "sidebar":
+      if let name = value as? String {
+        if let tabType = QuickSettingViewController.TabViewType(name: name) {
+          window.showSettingsSidebar(tab: tabType, force: true, hideIfAlreadyShown: false)
+        } else if let tabType = PlaylistViewController.TabViewType(name: name) {
+          window.showPlaylistSidebar(tab: tabType, force: true, hideIfAlreadyShown: false)
+        } else {
+          log("core.window.sidebar: Unknown sidebar name \"\(name)\"", level: .error)
+        }
+      } else {
+        window.hideSideBar(animate: true)
+      }
     default:
       log("core.window: \(prop) is not accessible", level: .warning)
     }

iina/JavascriptAPISidebarView.swift
@@ -1,0 +1,81 @@
+//
+//  JavascriptAPISidebarView.swift
+//  iina
+//
+//  Created by Collider LI on 11/10/2020.
+//  Copyright © 2020 lhc. All rights reserved.
+//
+
+import Foundation
+import JavaScriptCore
+import WebKit
+
+
+@objc protocol JavascriptAPISidebarViewExportable: JSExport {
+  func loadFile(_ path: String)
+  func show()
+  func hide()
+  func sendMessage(_ name: String, _ data: JSValue)
+  func onMessage(_ name: String, _ callback: JSValue)
+}
+
+class JavascriptAPISidebarView: JavascriptAPI, JavascriptAPISidebarViewExportable, WKScriptMessageHandler {
+  private var listeners: [String: JSManagedValue] = [:]
+
+  override func cleanUp(_ instance: JavascriptPluginInstance) {
+    listeners.removeAll()
+  }
+
+  func loadFile(_ path: String) {
+    let rootURL = pluginInstance.plugin.root
+    let url = rootURL.appendingPathComponent(path)
+    pluginInstance.sidebarTabView.load(URLRequest(url: url))
+  }
+
+  func show() {
+    let id = pluginInstance.plugin.identifier
+    player.mainWindow.showSettingsSidebar(tab: .plugin(id: id), force: true, hideIfAlreadyShown: false)
+  }
+
+  func hide() {
+    player.mainWindow.hideSideBar()
+  }
+
+  func sendMessage(_ name: String, _ data: JSValue) {
+    DispatchQueue.main.async {
+      let webView = self.pluginInstance.sidebarTabView
+      guard let object = data.toObject(),
+         let data = try? JSONSerialization.data(withJSONObject: object),
+         let dataString = String(data: data, encoding: .utf8) else {
+          webView.evaluateJavaScript("window.iina._emit(`\(name)`)")
+          return
+      }
+      webView.evaluateJavaScript("window.iina._emit(`\(name)`, `\(dataString)`)")
+    }
+  }
+
+  func onMessage(_ name: String, _ callback: JSValue) {
+    if let previousCallback = listeners[name] {
+      JSContext.current()!.virtualMachine.removeManagedReference(previousCallback, withOwner: self)
+    }
+    let managed = JSManagedValue(value: callback)
+    listeners[name] = managed
+    JSContext.current()!.virtualMachine.addManagedReference(managed, withOwner: self)
+
+  }
+
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+    guard let dict = message.body as? [Any], dict.count == 2,
+      let name = dict[0] as? String,
+      let callback = listeners[name] else { return }
+
+    guard let dataString = dict[1] as? String,
+      let data = dataString.data(using: .utf8),
+      let decoded = try? JSONSerialization.jsonObject(with: data) else {
+        callback.value.call(withArguments: [])
+      return
+    }
+
+    callback.value.call(withArguments: [JSValue(object: decoded, in: pluginInstance.js) ?? NSNull()])
+  }
+}

iina/JavascriptPlugin.swift
@@ -80,6 +80,7 @@ class JavascriptPlugin: NSObject {
   let domainList: [String]
 
   var subProviders: [[String: String]]?
+  let sidebarTabName: String?
 
   var entryURL: URL
   var preferencesPageURL: URL?
@@ -285,6 +286,12 @@ class JavascriptPlugin: NSObject {
     self.helpPage = jsonDict["helpPage"] as? String
     self.domainList = (jsonDict["allowedDomains"] as? [String]) ?? []
     self.subProviders = jsonDict["subtitleProviders"] as? [[String: String]]
+
+    if let sidebarTabDef = jsonDict["sidebarTab"] as? [String: String] {
+      self.sidebarTabName = sidebarTabDef["name"]
+    } else {
+      self.sidebarTabName = nil
+    }
 
     self.enabled = UserDefaults.standard.bool(forKey: "PluginEnabled." + identifier)
 

iina/JavascriptPluginInstance.swift
@@ -35,6 +35,7 @@ class JavascriptPluginInstance {
       "menu": JavascriptAPIMenu(context: ctx, pluginInstance: self),
       "overlay": JavascriptAPIOverlay(context: ctx, pluginInstance: self),
       "standaloneWindow": JavascriptAPIStandaloneWindow(context: ctx, pluginInstance: self),
+      "sidebar": JavascriptAPISidebarView(context: ctx, pluginInstance: self),
       "playlist": JavascriptAPIPlaylist(context: ctx, pluginInstance: self),
       "subtitle": JavascriptAPISubtitle(context: ctx, pluginInstance: self),
       "utils": JavascriptAPIUtils(context: ctx, pluginInstance: self),
@@ -67,6 +68,11 @@ class JavascriptPluginInstance {
     return window
   }()
   var standaloneWindowCreated = false
+
+  lazy var sidebarTabView: PluginSidebarView = {
+    let view = PluginSidebarView.create(pluginInstance: self)
+    return view
+  }()
 
   var menuItems: [JavascriptPluginMenuItem] = []
 

iina/MainWindowMenuActions.swift
@@ -11,68 +11,23 @@ import Cocoa
 extension MainWindowController {
 
   @objc func menuShowPlaylistPanel(_ sender: NSMenuItem) {
-    if sideBarStatus == .hidden || sideBarStatus == .settings {
-      playlistView.pleaseSwitchToTab(.playlist)
-      playlistButtonAction(sender)
-    } else {
-      if playlistView.currentTab != .playlist {
-        playlistView.pleaseSwitchToTab(.playlist)
-      } else {
-        playlistButtonAction(sender)
-      }
-    }
+    showPlaylistSidebar(tab: .playlist)
   }
 
   @objc func menuShowChaptersPanel(_ sender: NSMenuItem) {
-    if sideBarStatus == .hidden || sideBarStatus == .settings {
-      playlistView.pleaseSwitchToTab(.chapters)
-      playlistButtonAction(sender)
-    } else {
-      if playlistView.currentTab != .chapters {
-        playlistView.pleaseSwitchToTab(.chapters)
-      } else {
-        playlistButtonAction(sender)
-      }
-    }
+    showPlaylistSidebar(tab: .chapters)
   }
 
   @objc func menuShowVideoQuickSettings(_ sender: NSMenuItem) {
-    if sideBarStatus == .hidden || sideBarStatus == .playlist {
-      quickSettingView.pleaseSwitchToTab(.video)
-      settingsButtonAction(sender)
-    } else {
-      if quickSettingView.currentTab != .video {
-        quickSettingView.pleaseSwitchToTab(.video)
-      } else {
-        settingsButtonAction(sender)
-      }
-    }
+    showSettingsSidebar(tab: .video)
   }
 
   @objc func menuShowAudioQuickSettings(_ sender: NSMenuItem) {
-    if sideBarStatus == .hidden || sideBarStatus == .playlist {
-      quickSettingView.pleaseSwitchToTab(.audio)
-      settingsButtonAction(sender)
-    } else {
-      if quickSettingView.currentTab != .audio {
-        quickSettingView.pleaseSwitchToTab(.audio)
-      } else {
-        settingsButtonAction(sender)
-      }
-    }
+    showSettingsSidebar(tab: .audio)
   }
 
   @objc func menuShowSubQuickSettings(_ sender: NSMenuItem) {
-    if sideBarStatus == .hidden || sideBarStatus == .playlist {
-      quickSettingView.pleaseSwitchToTab(.sub)
-      settingsButtonAction(sender)
-    } else {
-      if quickSettingView.currentTab != .sub {
-        quickSettingView.pleaseSwitchToTab(.sub)
-      } else {
-        settingsButtonAction(sender)
-      }
-    }
+    showSettingsSidebar(tab: .sub)
   }
 
   @objc func menuChangeWindowSize(_ sender: NSMenuItem) {

commit 855027b6b105bd6a79f9fb964daa28ee0862138f

Fix a bug caused by 60b944f

iina/MainWindowController.swift
@@ -2598,15 +2598,15 @@ class MainWindowController: PlayerWindowController {
       if let tab = tab {
         view.pleaseSwitchToTab(tab)
       }
-      showSideBar(viewController: view, type: .settings)
-    case .playlist:
+      showSideBar(viewController: view, type: .playlist)
+    case .settings:
       if let tab = tab {
         view.pleaseSwitchToTab(tab)
       }
       hideSideBar {
-        self.showSideBar(viewController: view, type: .settings)
+        self.showSideBar(viewController: view, type: .playlist)
       }
-    case .settings:
+    case .playlist:
       if view.currentTab == tab {
         if hideIfAlreadyShown {
           hideSideBar()
e14284ab3009e6e78991a5de4fb7ffef8ced2a73
decodism <decodism@protonmail.com>
2022-10-18 02:58:09 +0200

    Fix CPU is consumed by OSC while shown and player paused

--- iina/PlayerCore.swift
+++ iina/PlayerCore.swift
@@ -1521,6 +1521,7 @@ class PlayerCore: NSObject {
 
   func createSyncUITimer() {
     invalidateTimer()
+    guard !info.isPaused else { return }
     syncPlayTimeTimer = Timer.scheduledTimer(
       timeInterval: TimeInterval(DurationDisplayTextField.precision >= 2 ? AppData.syncTimePreciseInterval : AppData.syncTimeInterval),
       target: self,

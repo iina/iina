1341a15544951ec0f07477a3f7d94c3d9bf07325
Matt Svoboda <matt.svoboda@gmail.com>
2022-11-01 21:41:03 -0700

    Fix clipping issues in Video Settings panel when Schow scrollbars="Always". Fix constraint errors, warnings and all other layout issues in Quick Settings panels. Merge Custom crop button into segmented control.

--- iina/PlaybackInfo.swift
+++ iina/PlaybackInfo.swift
@@ -88,7 +88,7 @@ class PlaybackInfo {
 
   /** The current applied aspect, used for find current aspect in menu, etc. Maybe not a good approach. */
   var unsureAspect: String = "Default"
-  var unsureCrop: String = "None"
+  var unsureCrop: String = "None" // TODO: rename this to "selectedCrop"
   var cropFilter: MPVFilter?
   var flipFilter: MPVFilter?
   var mirrorFilter: MPVFilter?

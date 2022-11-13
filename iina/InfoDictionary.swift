38e7c9322558106eedc2b84f3be9c486eabf3230
low-batt <86170219+low-batt@users.noreply.github.com>
2022-11-12 19:45:19 -0500

    Close add build info to About window, #3977
    Addressed review comments.
    
    This commit will:
    - Add a new class InfoDictionary that exposes some Info.plist entries as
      properties
    - Change AppDelegate, GuideWindowController, InitialWindowController,
      and JavascriptAPICore to use InfoDictionary instead of methods in
      the Utility class
    - Remove iinaCopyright and iinaVersion methods from Utility class
    - Add FFmpeg version to About window
    - Add build date and build branch as hidden entries in About window that
      are exposed via an option-click

--- iina/InfoDictionary.swift
+++ iina/InfoDictionary.swift
@@ -12,45 +12,38 @@ import Foundation
 ///
 /// This class exposes some of the entries contained in `Bundle.main.infoDictionary` as properties to provide for easier access
 /// to information contained in the dictionary from other classes.
-class InfoDictionary {
-  
+struct InfoDictionary {
+
   static let shared = InfoDictionary()
 
-  let buildBranch: String?
-  let buildCommit: String?
-  let buildDate: String?
-  
-  let bundleIdentifier: String
-  
-  var copyright: String { Bundle.main.infoDictionary!["NSHumanReadableCopyright"] as! String }
-
-  var version: (String, String) {
-    let infoDic = Bundle.main.infoDictionary!
-    return (infoDic["CFBundleShortVersionString"] as! String, infoDic["CFBundleVersion"] as! String)
-  }
-
-  private init() {
-    let infoDic = Bundle.main.infoDictionary!
-    bundleIdentifier = infoDic["CFBundleIdentifier"] as! String
-
-    // As recommended by Apple, IINA's custom Info.plist keys start with the bundle identifier.
-    let keyPrefix = bundleIdentifier + ".build"
-    buildBranch = infoDic["\(keyPrefix).branch"] as? String
-    buildCommit = infoDic["\(keyPrefix).commit"] as? String
-    
-    // Xcode refused to allow the build date in the Info.plist to use Date as the type because the
-    // value specified in the Info.plist is an identifier that is replaced at build time using the
-    // C preprocessor. So we need to convert from the ISO formatted string to a Date object.
+  var buildBranch: String? { dictionary["\(buildKeyPrefix).branch"] as? String }
+  var buildCommit: String? { dictionary["\(buildKeyPrefix).commit"] as? String }
+  var buildDate: String? {
     let fromString = ISO8601DateFormatter()
-    guard let date = infoDic["\(keyPrefix).date"] as? String,
+    guard let date = dictionary["\(buildKeyPrefix).date"] as? String,
           let dateObj = fromString.date(from: date) else {
-      buildDate = nil
-      return
+      return nil
     }
     // Use a localized date for the build date.
     let toString = DateFormatter()
     toString.dateStyle = .medium
     toString.timeStyle = .medium
-    buildDate = toString.string(from: dateObj)
+    return toString.string(from: dateObj)
+  }
+
+  private var buildKeyPrefix: String {
+    // As recommended by Apple, IINA's custom Info.plist keys start with the bundle identifier.
+    bundleIdentifier + ".build"
+  }
+
+  var bundleIdentifier: String { dictionary["CFBundleIdentifier"] as! String }
+
+  var copyright: String { dictionary["NSHumanReadableCopyright"] as! String }
+
+  let dictionary = Bundle.main.infoDictionary!
+
+  var version: (String, String) {
+    return (dictionary["CFBundleShortVersionString"] as! String,
+            dictionary["CFBundleVersion"] as! String)
   }
 }

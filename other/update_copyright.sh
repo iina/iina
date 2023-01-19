b713352619239e2fbc2f075e3dae0f4e62ac8cf1
low-batt <86170219+low-batt@users.noreply.github.com>
2023-01-19 14:36:55 -0500

    Update copyright to 2023
    The commit in the pull request will:
    - Change update_copyright.sh to not modify localized sources that must
      only be updated through Crowdin
    - Update the copyright displayed in the macOS "Get Info" window
    - Update the copyright displayed in the about window

--- other/update_copyright.sh
+++ other/update_copyright.sh
@@ -16,9 +16,19 @@ year=$(date +%Y)
 
 echo "Updating copyright year to $year"
 
+function update () {
+    local dir="$1"
+    local file="$2"
+    find "$(cd $srcdir/$dir; pwd)" -name "$file" -exec sed -i '' "s/ 2017-2[0-9]\{3\}/ 2017-$year/" {} +
+}
+
 # Update the copyright displayed in the macOS "Get Info" window for the application.
-find "$(cd $srcdir; pwd)" -name Info.plist -exec sed -i '' "s/ 2017-2[0-9]\{3\}/ 2017-$year/" {} +
+update . Info.plist
 
 # Update the copyright displayed in the about window.
-# This changes lots of files due to localization.
-find "$(cd $srcdir; pwd)" -name Contribution.rtf -exec sed -i '' "s/ 2017-2[0-9]\{3\}/ 2017-$year/" {} +
+# This copyright text is contained in Contribution.rtf which is localized.
+# To avoid conflicts when merging translations from Crowdin the procedure for
+# modifying localized source is to only update the base and English source in
+# GitHub. Changes for other languages must then be made using Crowdin.
+update Base.lproj Contribution.rtf
+update en.lproj Contribution.rtf

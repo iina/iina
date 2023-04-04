d5908fd82bd3809a05fd2392a1d0a9b3f171ce29
Saagar Jha <saagar@saagarjha.com>
2023-04-04 03:34:46 -0700

    Remove use of VLAs from +levDistance:and:
    Fixes #4314

--- iina/ObjcUtils.m
+++ iina/ObjcUtils.m
@@ -47,31 +47,34 @@ static inline int min(int a, int b, int c) {
 
 + (NSUInteger)levDistance:(NSString *)str0 and:(NSString *)str1 {
   int i, j;
-
+  
   str0 = [@" " stringByAppendingString:str0];
   str1 = [@" " stringByAppendingString:str1];
   const wchar_t *cstr0 = (const wchar_t *)[str0 cStringUsingEncoding:NSUTF32LittleEndianStringEncoding];
   const wchar_t *cstr1 = (const wchar_t *)[str1 cStringUsingEncoding:NSUTF32LittleEndianStringEncoding];
   size_t len0 = wcslen(cstr0);
   size_t len1 = wcslen(cstr1);
-
-  int dist[len0 + 1][len1 + 1];
+  
+  int *_dist = malloc(sizeof(int) * (len0 + 1) * (len1 + 1));
+  int (*dist)[len0 + 1][len1 + 1] = (int (*)[len0 + 1][len1 + 1])_dist;
   for (i = 0; i <= len0; ++i)
     for (j = 0; j <= len1; ++j)
-        dist[i][j] = 0;
-
+      (*dist)[i][j] = 0;
+  
   for (i = 1; i <= len0; ++i)
-    dist[i][0] = dist[i - 1][0] + INDEL_WEIGHT;
+    (*dist)[i][0] = (*dist)[i - 1][0] + INDEL_WEIGHT;
   for (j = 1; j <= len1; ++j)
-    dist[0][j] = dist[0][j - 1] + INDEL_WEIGHT;
-
+    (*dist)[0][j] = (*dist)[0][j - 1] + INDEL_WEIGHT;
+  
   for (j = 1; j <= len1; ++j)
     for (i = 1; i <= len0; ++i)
-        dist[i][j] = min(dist[i - 1][j] + INDEL_WEIGHT,
-                         dist[i][j - 1] + INDEL_WEIGHT,
-                         dist[i - 1][j - 1] + (cstr0[i] == cstr1[j] ? 0 : SUBSTITUTION_WEIGHT));
-
-  return dist[len0][len1];
+      (*dist)[i][j] = min((*dist)[i - 1][j] + INDEL_WEIGHT,
+                          (*dist)[i][j - 1] + INDEL_WEIGHT,
+                          (*dist)[i - 1][j - 1] + (cstr0[i] == cstr1[j] ? 0 : SUBSTITUTION_WEIGHT));
+  
+  int result = (*dist)[len0][len1];
+  free(_dist);
+  return result;
 }
 
 + (NSImage *)getImageFrom:(mpv_node *)image {

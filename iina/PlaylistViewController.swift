460e1514a77cfc1fa3fbdcfc5478261234c87e26
Matt Svoboda <matt.svoboda@gmail.com>
2023-04-22 20:34:19 -0700

    Fix missing text hightlight for Playlist tab when starting in Music Mode

--- iina/PlaylistViewController.swift
+++ iina/PlaylistViewController.swift
@@ -118,6 +118,9 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
     if pendingSwitchRequest != nil {
       switchToTab(pendingSwitchRequest!)
       pendingSwitchRequest = nil
+    } else {
+      // Initial display: need to draw highlight for currentTab
+      updateTabButtons(activeTab: currentTab)
     }
 
     // nofitications
@@ -221,18 +224,27 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
 
   /** Switch tab (for internal call) */
   private func switchToTab(_ tab: TabViewType) {
+    updateTabButtons(activeTab: tab)
     switch tab {
     case .playlist:
       tabView.selectTabViewItem(at: 0)
-      updateTabActiveStatus(for: playlistBtn, isActive: true)
-      updateTabActiveStatus(for: chaptersBtn, isActive: false)
     case .chapters:
       tabView.selectTabViewItem(at: 1)
-      updateTabActiveStatus(for: playlistBtn, isActive: false)
-      updateTabActiveStatus(for: chaptersBtn, isActive: true)
     }
 
     currentTab = tab
+  }
+
+  // Updates display of all tabs buttons to indicate that the given tab is active and the rest are not
+  private func updateTabButtons(activeTab: TabViewType) {
+    switch activeTab {
+    case .playlist:
+      updateTabActiveStatus(for: playlistBtn, isActive: true)
+      updateTabActiveStatus(for: chaptersBtn, isActive: false)
+    case .chapters:
+      updateTabActiveStatus(for: playlistBtn, isActive: false)
+      updateTabActiveStatus(for: chaptersBtn, isActive: true)
+    }
   }
 
   private func updateTabActiveStatus(for btn: NSButton, isActive: Bool) {

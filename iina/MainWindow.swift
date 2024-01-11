8e132369fc9fa04626450e0ad7c044cc731c13d5
Matt Svoboda <matt.svoboda@gmail.com>
2024-01-11 02:47:29 -0800

    Settings sidebars: prioritize arrow key navigation in text fields
    More detail:
    • Add check for arrow keys in text fields to MainWindow & send to the
      text field instead of handling as mpv binding
    • Change views in sidebars to refuse first responder so that tabbing out
      of a text field doesn't add an unwanted focus ring around them.

--- iina/MainWindow.swift
+++ iina/MainWindow.swift
@@ -25,6 +25,31 @@ class MainWindow: NSWindow {
     }
   }
 
+  override func performKeyEquivalent(with event: NSEvent) -> Bool {
+    /// AppKit by default will prioritize menu item key equivalents over arrow key navigation
+    /// (although for some reason it is the opposite for `ESC`, `TAB`, `ENTER` or `RETURN`).
+    /// Need to add an explicit check here for arrow keys to ensure that they always work when desired.
+    if let responder = firstResponder, shouldFavorArrowKeyNavigation(for: responder) {
+
+      let keyCode = KeyCodeHelper.mpvKeyCode(from: event)
+      let normalizedKeyCode = KeyCodeHelper.normalizeMpv(keyCode)
+
+      switch normalizedKeyCode {
+      case "UP", "DOWN", "LEFT", "RIGHT":
+        // Send arrow keys to view to enable key navigation
+        responder.keyDown(with: event)
+        return true
+      default:
+        break
+      }
+    }
+    return super.performKeyEquivalent(with: event)
+  }
+
+  private func shouldFavorArrowKeyNavigation(for responder: NSResponder) -> Bool {
+    return responder as? NSTextView != nil
+  }
+
   override var canBecomeKey: Bool {
     forceKeyAndMain ? true : super.canBecomeKey
   }

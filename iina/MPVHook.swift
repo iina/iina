fd13427b63323586393fcb56140d928f996ea394
Matt Svoboda <matt.svoboda@gmail.com>
2024-02-02 18:27:55 -0800

    Revert change to folder filtering + add special handling for shuffle arg instead (fixes regression: #4548) (#4566)
    * Handle folder filtering in IINA instead of sending to mpv, and intercept mpv shuffle arg & convert it to playlist-shuffle command. Fixes regression where folder filtering was broken, while still supporting shuffle via cmd-line
    
    * Change on_load hook to use main thread, based on feedback
    
    * Move shuffle playlist CLI logic from on_load hook to on_before_start_file hook
    
    * Refactor shuffle arg handling: add hook only if needed, and add earlier check whether it is still needed. Add some comments, cleanup

--- iina/MPVHook.swift
+++ iina/MPVHook.swift
@@ -15,6 +15,7 @@ struct MPVHook: RawRepresentable {
   init(_ string: String) { self.rawValue = string }
   init?(rawValue: RawValue) { self.rawValue = rawValue }
 
+  static let onBeforeStartFile = MPVHook("on_before_start_file")
   static let onLoad = MPVHook("on_load")
   static let onLoadFail = MPVHook("on_load_fail")
   static let onPreLoaded = MPVHook("on_preloaded")

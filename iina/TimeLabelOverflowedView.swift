fa376843f6c991950064056acd21f28033afb58e
low-batt <86170219+low-batt@users.noreply.github.com>
2024-05-24 21:04:27 -0400

    Fix RTL language causes OSC to malfunction, #4878 (#4879)
    This commit will:
    - Add initializers to TimeLabelOverflowedStackView,
      TimeLabelOverflowedView and TouchBarPlaySliderCell that set
      userInterfaceLayoutDirection to be leftToRight
    - Change MainWindowController.windowDidLoad to set the
      userInterfaceLayoutDirection property of fragControlView and
      oscFloatingTopView to be leftToRight
    - Change constraints from leading/trailing to left/right for several OSC
      components to prevent them from moving when a RTL language is used
    - Change MainWindowController methods mouseDown and mouseDragged to take
      into account the userInterfaceLayoutDirection setting during playlist
      resizing
    - Change ControlBarView.mouseDragged to take into account the
      userInterfaceLayoutDirection setting when repositioning the floating
      OSC
    - Rename the play property in the Constants struct to be
      blackRightPointingTriangle
    - Add a blackLeftPointingTriangle property to the Constants struct
    - Change PlaylistViewController.tableView to use a right or left arrow
      based on the userInterfaceLayoutDirection setting
    - Change the language-direction of the pip and volume related icons and
      the playlist icon from Fixed to Left to Right, Mirrors
    
    This adjusts IINA's behavior to match Apple's guidance that video
    controls and timeline indicators should not flip in a right-to-left
    language. Other controls, such as the volume control and the pip and
    playlist buttons are flipped for RTL languages.
    
    This also corrects problems dragging the floating OSC and resizing the
    playlist when a right to left language is configured.

--- iina/TimeLabelOverflowedView.swift
+++ iina/TimeLabelOverflowedView.swift
@@ -10,6 +10,15 @@ import Cocoa
 
 class TimeLabelOverflowedView: NSView {
 
+  /// Initializes and returns a newly allocated `TimeLabelOverflowedView` object from data in the specified coder object.
+  /// - Parameter coder: The coder object that contains the view’s configuration details.
+  /// - Important: As per Apple's [Internationalization and Localization Guide](https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/SupportingRight-To-LeftLanguages/SupportingRight-To-LeftLanguages.html)
+  ///     video controllers and timeline indicators should not flip in a right-to-left language. This can not be set in the XIB.
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    userInterfaceLayoutDirection = .leftToRight
+  }
+
   override var alignmentRectInsets: NSEdgeInsets {
     return NSEdgeInsets(top: 6, left: 0, bottom: 0, right: 0)
   }

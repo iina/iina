3ef397e05f330cc608d37354b434b89b8708cfdc
low-batt <86170219+low-batt@users.noreply.github.com>
2024-06-04 21:43:58 -0400

    Close enable screensaver while playing audio files, #4954
    This commit will:
    - Add new settings preventScreenSaver and
      allowScreenSaverForAudio
    - Change the PlayerCore checkCurrentMediaIsAudio method into an isAudio
      PlaybackInfo property
    - Add a "Prevent screen saver from starting while playing"
      checkbox with a "Not while in Music Mode or only playing audio"
      subordinate checkbox to the settings window General tab
    - Change the PlayerCore.checkStatusForSleep method to support the new
      settings
    - Change SleepPreventer to use ProcessInfo activities instead of
      IOPMAssertionCreateWithName
    - Add logging to SleepPreventer
    - Remove the "Cannot prevent display sleep!" alert along with the
      associated suppressCannotPreventDisplaySleep preference.
    
    This allows the user to control whether IINA prevents the screen saver
    from starting when in music mode or just playing audio or allow the
    screen saver to start and the display to power off and only prevent the
    system from sleeping.
    
    By default IINA will prevent the screen saver from starting when in
    music mode or playing audio to match the current behavior.

--- iina/Preference.swift
+++ iina/Preference.swift
@@ -73,8 +73,8 @@ struct Preference {
     /** Resume from last position */
     static let resumeLastPosition = Key("resumeLastPosition")
 
-    static let preventDisplaySleepForAudio = Key("preventDisplaySleepForAudio")
-    static let preventDisplaySleepForVideo = Key("preventDisplaySleepForVideo")
+    static let preventScreenSaver = Key("preventScreenSaver")
+    static let allowScreenSaverForAudio = Key("allowScreenSaverForAudio")
 
     static let alwaysFloatOnTop = Key("alwaysFloatOnTop")
     static let alwaysShowOnTopIcon = Key("alwaysShowOnTopIcon")
@@ -298,9 +298,6 @@ struct Preference {
 
     static let iinaLastPlayedFilePath = Key("iinaLastPlayedFilePath")
     static let iinaLastPlayedFilePosition = Key("iinaLastPlayedFilePosition")
-
-    /** Alerts */
-    static let suppressCannotPreventDisplaySleep = Key("suppressCannotPreventDisplaySleep")
 
     /** Internal */
     static let iinaEnablePluginSystem = Key("iinaEnablePluginSystem")
@@ -772,8 +769,8 @@ struct Preference {
     .legacyFullScreenAnimation: false,
     .showChapterPos: false,
     .resumeLastPosition: true,
-    .preventDisplaySleepForAudio: true,
-    .preventDisplaySleepForVideo: true,
+    .preventScreenSaver: true,
+    .allowScreenSaverForAudio: false,
     .useMediaKeys: true,
     .useAppleRemote: false,
     .alwaysFloatOnTop: false,
@@ -917,8 +914,6 @@ struct Preference {
     .watchProperties: [String](),
     .savedVideoFilters: [SavedFilter](),
     .savedAudioFilters: [SavedFilter](),
-
-    .suppressCannotPreventDisplaySleep: false,
 
     .enableRecentDocumentsWorkaround: false,
     .recentDocuments: [Any](),

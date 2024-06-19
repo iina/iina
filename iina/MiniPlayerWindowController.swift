iina/iina

commit 73a473edb9840e5a5a11089d6b6f21bb28e3e812

Display thumbnails in DAR instead of SAR (#4736)
* Display thumbnail in DAR instead of SAR
* Make MiniPlayer use DAR instead of SAR

iina/MiniPlayerWindowController.swift
@@ -288,7 +288,7 @@ class MiniPlayerWindowController: PlayerWindowController, NSPopoverDelegate {
 
   func updateVideoSize() {
     guard let window = window else { return }
-    let (width, height) = player.originalVideoSize
+    let (width, height) = player.videoSizeForDisplay
     let aspect = (width == 0 || height == 0) ? 1 : CGFloat(width) / CGFloat(height)
     let currentHeight = videoView.frame.height
     let newHeight = videoView.frame.width / aspect

iina/PlayerCore.swift
@@ -591,7 +591,7 @@ class PlayerCore: NSObject {
     miniPlayer.videoWrapperView.addSubview(videoView, positioned: .below, relativeTo: nil)
     Utility.quickConstraints(["H:|[v]|", "V:|[v]|"], ["v": videoView])
 
-    let (width, height) = originalVideoSize
+    let (width, height) = videoSizeForDisplay
     let aspect = (width == 0 || height == 0) ? 1 : CGFloat(width) / CGFloat(height)
     miniPlayer.updateVideoViewAspectConstraint(withAspect: aspect)
     miniPlayer.window?.layoutIfNeeded()

iina/MainWindowController.swift
@@ -2316,7 +2316,18 @@ class MainWindowController: PlayerWindowController {
       if player.info.thumbnailsReady, let image = player.info.getThumbnail(forSecond: previewTime.second)?.image {
         thumbnailPeekView.imageView.image = image.rotate(rotation)
         thumbnailPeekView.isHidden = false
-        let height = round(120 / thumbnailPeekView.imageView.image!.size.aspect)
+
+        // In some formats (like most of Japanese TV video formats), display aspect ratios (DAR) are different from the
+        // sample aspect ratio (SAR). A typical configuration is SAR 1440x1080i (4:3) w/ DAR 1920x1080 (16:9). Here we try
+        // to get the display aspect ratio from mpv to properly display the thumbnail.
+        let displayAspectRatio: CGFloat
+        if let width = player.info.displayWidth, let height = player.info.displayHeight {
+          displayAspectRatio = CGFloat(width) / CGFloat(height)
+        } else {
+          displayAspectRatio = thumbnailPeekView.imageView.image!.size.aspect
+        }
+
+        let height = round(120 / displayAspectRatio)
         let timePreviewFrameInWindow = timePreviewWhenSeek.superview!.convert(timePreviewWhenSeek.frame.origin, to: nil)
         let showAbove = canShowThumbnailAbove(timnePreviewYPos: timePreviewFrameInWindow.y, thumbnailHeight: height)
         let yPos = showAbove ? timePreviewFrameInWindow.y + timePreviewWhenSeek.frame.height : sliderFrameInWindow.y - height

iina/ThumbnailPeekView.swift
@@ -23,6 +23,7 @@ class ThumbnailPeekView: NSView {
     self.imageView.wantsLayer = true
     self.imageView.layer?.cornerRadius = 4
     self.imageView.layer?.masksToBounds = true
+    self.imageView.imageScaling = .scaleAxesIndependently
   }
 
 }

commit 005f8c73e6d92689d2011eb5a211a4cfb0e1f2fb

Fixed a regression bug where the default album art is incorrectly displayed
Caused by #4736

iina/MiniPlayerWindowController.swift
@@ -271,8 +271,8 @@ class MiniPlayerWindowController: PlayerWindowController, NSPopoverDelegate {
 
   func updateVideoSize() {
     guard let window = window else { return }
-    let (width, height) = player.videoSizeForDisplay
-    let aspect = (width == 0 || height == 0) ? 1 : CGFloat(width) / CGFloat(height)
+    let (width, height) = defaultAlbumArt.isHidden ? player.videoSizeForDisplay : (1, 1)
+    let aspect = CGFloat(width) / CGFloat(height)
     let currentHeight = videoView.frame.height
     let newHeight = videoView.frame.width / aspect
     updateVideoViewAspectConstraint(withAspect: aspect)

iina/PlayerCore.swift
@@ -588,15 +588,19 @@ class PlayerCore: NSObject {
     miniPlayer.videoWrapperView.addSubview(videoView, positioned: .below, relativeTo: nil)
     Utility.quickConstraints(["H:|[v]|", "V:|[v]|"], ["v": videoView])
 
-    let (width, height) = videoSizeForDisplay
-    let aspect = (width == 0 || height == 0) ? 1 : CGFloat(width) / CGFloat(height)
+    // if received video size before switching to music mode, hide default album art
+    let width, height: Int
+    if info.vid != 0 {
+      miniPlayer.defaultAlbumArt.isHidden = true
+      (width, height) = videoSizeForDisplay
+    } else {
+      (width, height) = (1, 1)
+    }
+
+    let aspect = CGFloat(width) / CGFloat(height)
     miniPlayer.updateVideoViewAspectConstraint(withAspect: aspect)
     miniPlayer.window?.layoutIfNeeded()
 
-    // if received video size before switching to music mode, hide default album art
-    if info.vid != 0 {
-      miniPlayer.defaultAlbumArt.isHidden = true
-    }
     // in case of video size changed, reset mini player window size if playlist is folded
     if !miniPlayer.isPlaylistVisible {
       miniPlayer.setToInitialWindowSize(display: true, animate: false)
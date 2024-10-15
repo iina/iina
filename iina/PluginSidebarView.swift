61f6496050e7c80eeb49c385000fb8e8b0bd638a
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2024-10-15 01:17:47 -0700

    Fix XCode 16 warnings & improve Set as Default for MacOS 11+
    This includes:
    - Add @preconcurrency annotation to WebKit import to fix warning
    - Create Utility.icon() which uses UTTypes, to quash deprecation
      warning about NSWorkspace.shared.icon
    - Trivial updates to fix @Sendable warnings
    - Minor refactor to use NSShadow.shadowColor instead of deprecated
      NSColor.controlShadowColor in PlaySliderCell
    - Refactor PrefUtilsViewController.setAsDefaultOKBtnAction logic to
      use UTTypes for MacOS 11+, and ensure that all UT identifiers for
      all supported suffixes are changed to IINA

--- iina/PluginSidebarView.swift
+++ iina/PluginSidebarView.swift
@@ -7,7 +7,7 @@
 //
 
 import Cocoa
-import WebKit
+@preconcurrency import WebKit
 
 class PluginSidebarView: WKWebView, WKNavigationDelegate {
   weak private var pluginInstance: JavascriptPluginInstance!

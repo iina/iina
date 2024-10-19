641b5163b6e07a1b60befb7029ba3d31bb8907b1
Yuze Jiang <i@uiryuu.moe>
2024-10-19 10:32:48 +0900

    Fix typo

--- iina/LogWindowController.swift
+++ iina/LogWindowController.swift
@@ -130,7 +130,7 @@ class LogWindowController: NSWindowController, NSMenuDelegate {
       self.logs.append(contentsOf: logs)
       logs.removeAll()
       if scroll {
-        // macOS couldn't calcuate the frame size correctly when the row height is variable and
+        // macOS couldn't calculate the frame size correctly when the row height is variable and
         // is not rendered. After the first scroll, all rows should be rendered, which makes the
         // second frame size correct. Scroll the second time to correctly scroll to the last row.
         logTableView.scroll(NSPoint(x: 0, y: logTableView.frame.size.height))

hyperphonic0/iina-advance

Workaround for imprecise Double values for speed provided by mpv
 • Rename Double's .string property to .stringWithMaxFractionDigits6
 • Reduce its maximumFractionDigits from 15 to 6
 • Remove use of maximumSignificantDigits (spurious: usesSignificantDigits==false)

iina/MenuController.swift
@@ -491,7 +491,7 @@ class MenuController: NSObject, NSMenuDelegate {
     let isDisplayingChapters = player.windowController.isShowing(sidebarTab: .chapters)
     chapterPanel?.title = isDisplayingChapters ? Constants.String.hideChaptersPanel : Constants.String.chaptersPanel
     pause.title = player.info.isPaused ? Constants.String.resume : Constants.String.pause
-    let speed = player.info.playSpeed.string
+    let speed = player.info.playSpeed.stringWithMaxFractionDigits6
     speedIndicator.title = String(format: NSLocalizedString("menu.speed", comment: "Speed:"), speed)
     player.mpv.queue.async { [self] in
       guard player.isActive else { return }
@@ -1103,7 +1103,7 @@ class MenuController: NSObject, NSMenuDelegate {
       case "speed_up",
         "speed_down":
         // Title format expects arg type: String
-        valObj = abs(value).string
+        valObj = abs(value).stringWithMaxFractionDigits6
       default:
         // Title format expects numeric arg
         valObj = abs(value)

iina/iina

 • Rename Double's .string property to .groupedStringUpTo6Decimals

iina/MenuController.swift
@@ -489,7 +489,7 @@ class MenuController: NSObject, NSMenuDelegate {
     let loopMode = player.getLoopMode()
     fileLoop.state = loopMode == .file ? .on : .off
     playlistLoop.state = loopMode == .playlist ? .on : .off
-    let speed = player.info.playSpeed.string
+    let speed = player.info.playSpeed.groupedStringUpTo6Decimals
     speedIndicator.title = String(format: NSLocalizedString("menu.speed", comment: "Speed:"), speed)
   }
 
@@ -954,7 +954,7 @@ class MenuController: NSObject, NSMenuDelegate {
       case "speed_up",
         "speed_down":
         // Title format expects arg type: String
-        valObj = abs(value).string
+        valObj = abs(value).groupedStringUpTo6Decimals
       default:
         // Title format expects numeric arg
         valObj = abs(value)
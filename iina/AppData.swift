5e19fce5e47a3b35fb43a20b47e772d8401f15cf
low-batt <86170219+low-batt@users.noreply.github.com>
2025-09-03 18:31:38 -0400

    Close gapless audio playback, #5433
    This commit will add a new "Gapless audio" setting in the "Audio"
    section on the "Video/Audio" tab of IINA's settings.
    
    Previously gapless audio did not work because IINA always paused
    playback when loading a file. This was a workaround added a long time
    ago for a problem that is no longer reproducible. Commit 9f556bf removed
    the workaround allowing gapless audio to work. This commit provides a UI
    to control it.

--- iina/AppData.swift
+++ iina/AppData.swift
@@ -67,6 +67,7 @@ struct AppData {
   static let algorithmHelpLink = "https://mpv.io/manual/stable/#options-tone-mapping"
   static let disableAnimationsHelpLink = "https://developer.apple.com/design/human-interface-guidelines/accessibility#Motion"
   static let gainAdjustmentHelpLink = "https://mpv.io/manual/stable/#options-replaygain"
+  static let gaplessAudioHelpLink = "https://mpv.io/manual/stable/#options-gapless-audio"
   static let audioDriverHellpLink = "https://mpv.io/manual/stable/#audio-output-drivers-coreaudio"
 
   static let widthWhenNoVideo = 640

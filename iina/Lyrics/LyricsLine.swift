077206935d5fdfdc35cacd2ebab78ad35fc8f4d7
EM <efmolina@iastate.edu>
2025-12-21 20:28:19 -0600

    Add LyricsLine model for synced lyrics

---
+++ iina/Lyrics/LyricsLine.swift
@@ -0,0 +1,14 @@
+//
+//  LyricsLine.swift
+//  iina
+//
+//  Created by Erik Molina on 12/21/25.
+//  Copyright © 2025 lhc. All rights reserved.
+//
+
+import Foundation
+
+struct LyricsLine {
+    let time: TimeInterval
+    let text: String
+}

004ef414e629bd0adfefe2c19c4c93f8c0556bc6
LH-and-FPGA <180024497+LH-and-FPGA@users.noreply.github.com>
2026-04-15 23:47:01 +0100

    Use standard NSSliderCell for volume slider on macOS 26 for full Liquid Glass knob animation

--- iina/VolumeSlider.swift
+++ iina/VolumeSlider.swift
@@ -12,6 +12,23 @@ import Cocoa
 /// for the volume slider in the on screen controller.
 class VolumeSlider: NSSlider {
 
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    if #available(macOS 26, *) {
+      // Replace custom cell with standard NSSliderCell for full Liquid Glass animation
+      let maxVol = Double(Preference.integer(for: .maxVolume))
+      let standardCell = NSSliderCell()
+      standardCell.minValue = 0
+      standardCell.maxValue = maxVol
+      standardCell.doubleValue = self.doubleValue
+      standardCell.isContinuous = self.isContinuous
+      standardCell.target = self.target
+      standardCell.action = self.action
+      standardCell.controlSize = (self.cell as? NSCell)?.controlSize ?? .regular
+      self.cell = standardCell
+    }
+  }
+
   // MARK: - Mouse / Trackpad events
 
   /// The user is scrolling while the cursor is within the slider.
